15315ded0ca2ffb5eb0fe6c888fec3a942f5e560
Pascal Horton <pascal.horton@terranum.ch>
2013-05-22 11:13:21 +0200

    Commenting of unsused experimental code.

--- src/shared_processing/core/asPostprocessor.h
+++ src/shared_processing/core/asPostprocessor.h
@@ -1,17 +1,17 @@
-/** 
+/**
  *
  *  This file is part of the AtmoSwing software.
  *
- *  Copyright (c) 2008-2012  University of Lausanne, Pascal Horton (pascal.horton@unil.ch). 
+ *  Copyright (c) 2008-2012  University of Lausanne, Pascal Horton (pascal.horton@unil.ch).
  *  All rights reserved.
  *
- *  THIS CODE, SOFTWARE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY  
+ *  THIS CODE, SOFTWARE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY
  *  OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
  *  PURPOSE.
  *
  */
- 
+
 #ifndef ASPOSTPROCESSOR_H
 #define ASPOSTPROCESSOR_H
 
@@ -29,13 +29,13 @@ public:
         DuplicationOnCriteria, // Duplication of the analog days based on the criteria
         DuplicationOnCriteriaExponent // Duplication of the analog days based on the criteria with an exponent
     };
-
+/*
     static Array1DFloat Postprocess(const Array1DFloat &analogsValues, const Array1DFloat &analogsCriteria, asParametersScoring &params);
 
 
     static Array1DFloat PostprocessDuplicationOnCriteria(const Array1DFloat &analogsValues, const Array1DFloat &analogsCriteria);
     static Array1DFloat PostprocessDuplicationOnCriteriaExponent(const Array1DFloat &analogsValues, const Array1DFloat &analogsCriteria, asParametersScoring &params);
-
+*/
 
 protected:
 private:
