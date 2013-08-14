01b91be091bdd3d2e849b2382cf5abdde05fb45f
Pascal Horton <pascal.horton@terranum.ch>
2013-08-14 10:54:57 +0200

    Changing files header text.

--- src/app_calibrator/app/AtmoswingAppCalibrator.cpp
+++ src/app_calibrator/app/AtmoswingAppCalibrator.cpp
@@ -1,11 +1,16 @@
-/***************************************************************
- * Name:      AtmoswingAppCalibrator.cpp
- * Purpose:   Code for Application Class
- * Author:    Pascal Horton (pascal.horton@unil.ch)
- * Created:   2009-06-08
- * Copyright: Pascal Horton (www.unil.ch/igar)
- * License:
- **************************************************************/
+/**
+ *
+ *  This file is part of the AtmoSwing software.
+ *
+ *  Copyright (c) 2008-2012  University of Lausanne, Pascal Horton (pascal.horton@unil.ch).
+ *  All rights reserved.
+ *
+ *  THIS CODE, SOFTWARE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY
+ *  OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ *  PURPOSE.
+ *
+ */
 
 #ifdef WX_PRECOMP
 #include "wx_pch.h"
