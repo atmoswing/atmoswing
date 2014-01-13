12154bb81287e06b616ca6f6ae4894c057355252
Pascal Horton <pascal.horton@terranum.ch>
2014-01-13 18:19:50 +0100

    Opened forecasts are now correctly closed in the destructor.

--- src/app_viewer/core/asForecastManager.cpp
+++ src/app_viewer/core/asForecastManager.cpp
@@ -40,7 +40,7 @@ asForecastManager::asForecastManager(wxWindow* parent)
 
 asForecastManager::~asForecastManager()
 {
-    //dtor
+    ClearArrays();
 }
 
 void asForecastManager::AddDirectoryPastForecasts(const wxString &dir)
@@ -92,7 +92,11 @@ bool asForecastManager::Open(const wxString &filePath, bool doRefresh)
     // Create and load the forecast
     asResultsAnalogsForecast* forecast = new asResultsAnalogsForecast(wxEmptyString);
 
-    if(!forecast->Load(filePath)) return false;
+    if(!forecast->Load(filePath)) 
+    {
+        wxDELETE(forecast);
+        return false;
+    }
 
     // Check the lead time origin
     if( (m_LeadTimeOrigin!=0) && (forecast->GetLeadTimeOrigin()!=m_LeadTimeOrigin) )
