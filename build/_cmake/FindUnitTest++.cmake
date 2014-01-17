254ffbe616094993d18fb2ebfeb1cff7e13275ad
Pascal Horton <pascal.horton@terranum.ch>
2014-01-17 14:28:19 +0100

    Improvement of the CMake files and increase of the VS compiler warning level.

--- build/_cmake/FindUnitTest++.cmake
+++ build/_cmake/FindUnitTest++.cmake
@@ -1,33 +1,48 @@
-# Try to find UnitTest++
+# Find UnitTest++
 #
 #
 
 SET (UNITTEST++_FOUND FALSE)
+SET (UNITTEST++_LIBRARIES    "")
 
 FIND_PATH (UNITTEST++_INCLUDE_DIR UnitTest++.h
 	/usr/include/unittest++ 
+	/usr/include/UnitTest++ 
 	/usr/local/include/unittest++ 
+	/usr/local/include/UnitTest++ 
 	/opt/local/include/unittest++
+	/opt/local/include/UnitTest++
 	$ENV{UNITTESTXX_PATH}/src 
 	$ENV{UNITTESTXX_INCLUDE_PATH}
 	)
 
-FIND_LIBRARY (UNITTEST++_LIBRARY NAMES UnitTest++ PATHS 
+FIND_LIBRARY (UNITTEST++_LIBRARY_RELEASE NAMES UnitTest++ PATHS 
 	/usr/lib 
 	/usr/local/lib 
 	/opt/local/lib 
-	$ENV{UNITTESTXX_PATH} 
-	ENV{UNITTESTXX_LIBRARY_PATH}
 	${UNITTEST++_INCLUDE_DIR}/../lib/Release
 	)
+mark_as_advanced(UNITTEST++_LIBRARY_RELEASE)
 
-IF (UNITTEST++_INCLUDE_DIR AND UNITTEST++_LIBRARY)
+FIND_LIBRARY (UNITTEST++_LIBRARY_DEBUG NAMES UnitTest++ PATHS 
+	/usr/lib 
+	/usr/local/lib 
+	/opt/local/lib 
+	${UNITTEST++_INCLUDE_DIR}/../lib/Debug
+	)
+mark_as_advanced(UNITTEST++_LIBRARY_DEBUG)
+
+IF (UNITTEST++_INCLUDE_DIR AND UNITTEST++_LIBRARY_RELEASE AND UNITTEST++_LIBRARY_DEBUG)
+	list(APPEND UNITTEST++_LIBRARIES
+		debug ${UNITTEST++_LIBRARY_DEBUG} optimized ${UNITTEST++_LIBRARY_RELEASE}
+		)
 	SET (UNITTEST++_FOUND TRUE)
-ENDIF (UNITTEST++_INCLUDE_DIR AND UNITTEST++_LIBRARY)
+ENDIF (UNITTEST++_INCLUDE_DIR AND UNITTEST++_LIBRARY_RELEASE AND UNITTEST++_LIBRARY_DEBUG)
 
 IF (UNITTEST++_FOUND)
    IF (NOT UnitTest++_FIND_QUIETLY)
-      MESSAGE(STATUS "Found UnitTest++: ${UNITTEST++_LIBRARY}")
+      MESSAGE(STATUS "Found UnitTest++ Release : ${UNITTEST++_LIBRARY_RELEASE}")
+      MESSAGE(STATUS "Found UnitTest++ Debug: ${UNITTEST++_LIBRARY_DEBUG}")
    ENDIF (NOT UnitTest++_FIND_QUIETLY)
 ELSE (UNITTEST++_FOUND)
    IF (UnitTest++_FIND_REQUIRED)
