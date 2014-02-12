31e778be5db5896d8f2f5c57682cd08f96f3ce67
Pascal Horton <pascalhorton@users.noreply.github.com>
2014-02-12 10:09:02 +0100

    Fix of errors related to optional parameters

--- src/app_calibrator/core/asParametersCalibration.cpp
+++ src/app_calibrator/core/asParametersCalibration.cpp
@@ -343,7 +343,7 @@ bool asParametersCalibration::LoadFromFile(const wxString &filePath)
     if(!SetForecastScoreAnalogsNumberVector(GetFileParamIntVector(fileParams, "AnalogsNumber"))) return false;
     if(!fileParams.GoANodeBack()) return false;
 
-    if(fileParams.GoToChildNodeWithAttributeValue("name", "Postprocessing"))
+    if(fileParams.GoToChildNodeWithAttributeValue("name", "Postprocessing", asHIDE_WARNINGS))
     {
         SetForecastScorePostprocess(1);
         if(!SetForecastScorePostprocessMethod(fileParams.GetFirstElementAttributeValueText("Method", "value"))) return false;
