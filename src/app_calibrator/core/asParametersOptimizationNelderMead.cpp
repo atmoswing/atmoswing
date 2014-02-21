cec7b187c435f2297c6922da13f6a4edca75e9fb
Pascal Horton <pascalhorton@users.noreply.github.com>
2014-02-21 11:37:12 +0100

    Build succeeds.

--- src/app_calibrator/core/asParametersOptimizationNelderMead.cpp
+++ src/app_calibrator/core/asParametersOptimizationNelderMead.cpp
@@ -30,7 +30,7 @@ void asParametersOptimizationNelderMead::SetMeans(std::vector <asParametersOptim
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Uptsnb) SetPredictorUptsnb(i_step, i_ptor, 0);
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vmin) SetPredictorVmin(i_step, i_ptor, 0);
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vptsnb) SetPredictorVptsnb(i_step, i_ptor, 0);
-            if(!m_StepsLocks[i_step].Predictors[i_ptor].DTimeHours) SetPredictorDTimeHours(i_step, i_ptor, 0);
+            if(!m_StepsLocks[i_step].Predictors[i_ptor].TimeHours) SetPredictorTimeHours(i_step, i_ptor, 0);
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Weight) SetPredictorWeight(i_step, i_ptor, 0);
         }
     }
@@ -51,7 +51,7 @@ void asParametersOptimizationNelderMead::SetMeans(std::vector <asParametersOptim
                 if(!m_StepsLocks[i_step].Predictors[i_ptor].Uptsnb) SetPredictorUptsnb(i_step, i_ptor, GetPredictorUptsnb(i_step, i_ptor) + vParameters[i_set].GetPredictorUptsnb(i_step, i_ptor));
                 if(!m_StepsLocks[i_step].Predictors[i_ptor].Vmin) SetPredictorVmin(i_step, i_ptor, GetPredictorVmin(i_step, i_ptor) + vParameters[i_set].GetPredictorVmin(i_step, i_ptor));
                 if(!m_StepsLocks[i_step].Predictors[i_ptor].Vptsnb) SetPredictorVptsnb(i_step, i_ptor, GetPredictorVptsnb(i_step, i_ptor) + vParameters[i_set].GetPredictorVptsnb(i_step, i_ptor));
-                if(!m_StepsLocks[i_step].Predictors[i_ptor].DTimeHours) SetPredictorDTimeHours(i_step, i_ptor, GetPredictorDTimeHours(i_step, i_ptor) + vParameters[i_set].GetPredictorDTimeHours(i_step, i_ptor));
+                if(!m_StepsLocks[i_step].Predictors[i_ptor].TimeHours) SetPredictorTimeHours(i_step, i_ptor, GetPredictorTimeHours(i_step, i_ptor) + vParameters[i_set].GetPredictorTimeHours(i_step, i_ptor));
                 if(!m_StepsLocks[i_step].Predictors[i_ptor].Weight) SetPredictorWeight(i_step, i_ptor, GetPredictorWeight(i_step, i_ptor) + vParameters[i_set].GetPredictorWeight(i_step, i_ptor));
             }
         }
@@ -71,7 +71,7 @@ void asParametersOptimizationNelderMead::SetMeans(std::vector <asParametersOptim
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Uptsnb) SetPredictorUptsnb(i_step, i_ptor, (int)((double)GetPredictorUptsnb(i_step, i_ptor) / (double)elementsNb));
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vmin) SetPredictorVmin(i_step, i_ptor, GetPredictorVmin(i_step, i_ptor) / (double)elementsNb);
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vptsnb) SetPredictorVptsnb(i_step, i_ptor, (int)((double)GetPredictorVptsnb(i_step, i_ptor) / (double)elementsNb));
-            if(!m_StepsLocks[i_step].Predictors[i_ptor].DTimeHours) SetPredictorDTimeHours(i_step, i_ptor, GetPredictorDTimeHours(i_step, i_ptor) / (double)elementsNb);
+            if(!m_StepsLocks[i_step].Predictors[i_ptor].TimeHours) SetPredictorTimeHours(i_step, i_ptor, GetPredictorTimeHours(i_step, i_ptor) / (double)elementsNb);
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Weight) SetPredictorWeight(i_step, i_ptor, GetPredictorWeight(i_step, i_ptor) / (double)elementsNb);
         }
     }
@@ -95,13 +95,12 @@ void asParametersOptimizationNelderMead::GeometricTransform(asParametersOptimiza
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Uptsnb) SetPredictorUptsnb(i_step, i_ptor, refParams.GetPredictorUptsnb(i_step, i_ptor)+coefficient*(refParams.GetPredictorUptsnb(i_step, i_ptor)-GetPredictorUptsnb(i_step,i_ptor)));
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vmin) SetPredictorVmin(i_step, i_ptor, refParams.GetPredictorVmin(i_step, i_ptor)+coefficient*(refParams.GetPredictorVmin(i_step, i_ptor)-GetPredictorVmin(i_step,i_ptor)));
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vptsnb) SetPredictorVptsnb(i_step, i_ptor, refParams.GetPredictorVptsnb(i_step, i_ptor)+coefficient*(refParams.GetPredictorVptsnb(i_step, i_ptor)-GetPredictorVptsnb(i_step,i_ptor)));
-            if(!m_StepsLocks[i_step].Predictors[i_ptor].DTimeHours) SetPredictorDTimeHours(i_step, i_ptor, refParams.GetPredictorDTimeHours(i_step, i_ptor)+coefficient*(refParams.GetPredictorDTimeHours(i_step, i_ptor)-GetPredictorDTimeHours(i_step,i_ptor)));
+            if(!m_StepsLocks[i_step].Predictors[i_ptor].TimeHours) SetPredictorTimeHours(i_step, i_ptor, refParams.GetPredictorTimeHours(i_step, i_ptor)+coefficient*(refParams.GetPredictorTimeHours(i_step, i_ptor)-GetPredictorTimeHours(i_step,i_ptor)));
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Weight) SetPredictorWeight(i_step, i_ptor, refParams.GetPredictorWeight(i_step, i_ptor)+coefficient*(refParams.GetPredictorWeight(i_step, i_ptor)-GetPredictorWeight(i_step,i_ptor)));
         }
     }
     if(!m_ForecastScoreLocks.AnalogsNumber) SetForecastScoreAnalogsNumber(refParams.GetForecastScoreAnalogsNumber()+coefficient*(refParams.GetForecastScoreAnalogsNumber()-GetForecastScoreAnalogsNumber()));
 
-    FixTimeShift();
     FixWeights();
     FixCoordinates();
     CheckRange();
@@ -123,13 +122,12 @@ void asParametersOptimizationNelderMead::Reduction(asParametersOptimizationNelde
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Uptsnb) SetPredictorUptsnb(i_step, i_ptor, refParams.GetPredictorUptsnb(i_step, i_ptor)+sigma*(GetPredictorUptsnb(i_step, i_ptor)-refParams.GetPredictorUptsnb(i_step, i_ptor)));
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vmin) SetPredictorVmin(i_step, i_ptor, refParams.GetPredictorVmin(i_step, i_ptor)+sigma*(GetPredictorVmin(i_step, i_ptor)-refParams.GetPredictorVmin(i_step, i_ptor)));
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Vptsnb) SetPredictorVptsnb(i_step, i_ptor, refParams.GetPredictorVptsnb(i_step, i_ptor)+sigma*(GetPredictorVptsnb(i_step, i_ptor)-refParams.GetPredictorVptsnb(i_step, i_ptor)));
-            if(!m_StepsLocks[i_step].Predictors[i_ptor].DTimeHours) SetPredictorDTimeHours(i_step, i_ptor, refParams.GetPredictorDTimeHours(i_step, i_ptor)+sigma*(GetPredictorDTimeHours(i_step, i_ptor)-refParams.GetPredictorDTimeHours(i_step, i_ptor)));
+            if(!m_StepsLocks[i_step].Predictors[i_ptor].TimeHours) SetPredictorTimeHours(i_step, i_ptor, refParams.GetPredictorTimeHours(i_step, i_ptor)+sigma*(GetPredictorTimeHours(i_step, i_ptor)-refParams.GetPredictorTimeHours(i_step, i_ptor)));
             if(!m_StepsLocks[i_step].Predictors[i_ptor].Weight) SetPredictorWeight(i_step, i_ptor, refParams.GetPredictorWeight(i_step, i_ptor)+sigma*(GetPredictorWeight(i_step, i_ptor)-refParams.GetPredictorWeight(i_step, i_ptor)));
         }
     }
     if(!m_ForecastScoreLocks.AnalogsNumber) SetForecastScoreAnalogsNumber(refParams.GetForecastScoreAnalogsNumber()+sigma*(GetForecastScoreAnalogsNumber()-refParams.GetForecastScoreAnalogsNumber()));
 
-    FixTimeShift();
     FixWeights();
     FixCoordinates();
     CheckRange();
