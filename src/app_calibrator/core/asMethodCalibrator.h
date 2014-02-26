561c483e70d69ff6e75c767178127e0832be19b9
Pascal Horton <pascalhorton@users.noreply.github.com>
2014-02-26 13:49:16 +0100

    The data loading now takes place in a shared separate function.

--- src/app_calibrator/core/asMethodCalibrator.h
+++ src/app_calibrator/core/asMethodCalibrator.h
@@ -8,24 +8,24 @@
  * You can read the License at http://opensource.org/licenses/CDDL-1.0
  * See the License for the specific language governing permissions
  * and limitations under the License.
- * 
- * When distributing Covered Code, include this CDDL Header Notice in 
- * each file and include the License file (licence.txt). If applicable, 
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in
+ * each file and include the License file (licence.txt). If applicable,
  * add the following below this CDDL Header, with the fields enclosed
  * by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- * 
- * The Original Software is AtmoSwing. The Initial Developer of the 
- * Original Software is Pascal Horton of the University of Lausanne. 
+ *
+ * The Original Software is AtmoSwing. The Initial Developer of the
+ * Original Software is Pascal Horton of the University of Lausanne.
  * All Rights Reserved.
- * 
+ *
  */
 
 /*
  * Portions Copyright 2008-2013 University of Lausanne.
  * Portions Copyright 2013 Pascal Horton, Terr@num.
  */
- 
+
 #ifndef ASMETHODCALIBRATOR_H
 #define ASMETHODCALIBRATOR_H
 
@@ -126,6 +126,7 @@ protected:
 
 	virtual bool Calibrate(asParametersCalibration &params) = 0;
 	bool PreloadData(asParametersScoring &params);
+	bool LoadData(asParametersScoring &params, int i_step, double timeStartData, double timeEndData);
 
 private:
 
