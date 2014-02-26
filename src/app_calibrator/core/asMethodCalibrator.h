110c192c7816ea5e74c4a8ed8997c9ae87bab498
Pascal Horton <pascal.horton@terranum.ch>
2014-02-26 16:38:46 +0100

    Fix of a bug in the calibrator.

--- src/app_calibrator/core/asMethodCalibrator.h
+++ src/app_calibrator/core/asMethodCalibrator.h
@@ -125,7 +125,7 @@ protected:
 
 	virtual bool Calibrate(asParametersCalibration &params) = 0;
 	bool PreloadData(asParametersScoring &params);
-	bool LoadData(std::vector < asDataPredictor* > predictors, asParametersScoring &params, int i_step, double timeStartData, double timeEndData);
+	bool LoadData(std::vector < asDataPredictor* > &predictors, asParametersScoring &params, int i_step, double timeStartData, double timeEndData);
 
 private:
 
