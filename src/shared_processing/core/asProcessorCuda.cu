ff565b70ce4d233144214fbc65b1187ab80ca122
Pascal Horton <pascalhorton@users.noreply.github.com>
2014-04-04 17:53:02 +0200

    Commenting cudaDeviceReset call.

--- src/shared_processing/core/asProcessorCuda.cu
+++ src/shared_processing/core/asProcessorCuda.cu
@@ -250,10 +250,10 @@ bool asProcessorCuda::ProcessCriteria(std::vector < float* > &vpTargData,
 
 	// cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
+    /*cudaStatus = cudaDeviceReset();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceReset failed!");
-    }
+    }*/
 	
 	return true;
 }
