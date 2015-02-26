9f04bb7cb4506ad7802014aa5205bdab009b6b0e
Pascal Horton <pascal.horton@terranum.ch>
2015-02-26 16:25:18 +0100

    Removing any Proj4 dependency.

--- test/src/asGeoTest.cpp
+++ test/src/asGeoTest.cpp
@@ -32,10 +32,10 @@
 
 namespace
 {
-/*
+
 TEST(CheckPointWGS84True)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 10;
     Point.y = 10;
@@ -45,7 +45,7 @@ TEST(CheckPointWGS84True)
 
 TEST(CheckPointWGS84UVMaxTrue)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 360;
     Point.y = 90;
@@ -55,7 +55,7 @@ TEST(CheckPointWGS84UVMaxTrue)
 
 TEST(CheckPointWGS84UVMinTrue)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 0;
     Point.y = -90;
@@ -65,7 +65,7 @@ TEST(CheckPointWGS84UVMinTrue)
 
 TEST(CheckPointWGS84UTooHigh)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 360.1;
     Point.y = 10;
@@ -76,7 +76,7 @@ TEST(CheckPointWGS84UTooHigh)
 
 TEST(CheckPointWGS84UTooLow)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = -0.1;
     Point.y = 10;
@@ -87,7 +87,7 @@ TEST(CheckPointWGS84UTooLow)
 
 TEST(CheckPointWGS84VTooHigh)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 10;
     Point.y = 90.1;
@@ -98,7 +98,7 @@ TEST(CheckPointWGS84VTooHigh)
 
 TEST(CheckPointWGS84VTooLow)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 10;
     Point.y = -90.1;
@@ -109,7 +109,7 @@ TEST(CheckPointWGS84VTooLow)
 
 TEST(CheckPointWGS84UTooHighCorr)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 360.1;
     Point.y = 10;
@@ -120,7 +120,7 @@ TEST(CheckPointWGS84UTooHighCorr)
 
 TEST(CheckPointWGS84UTooLowCorr)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = -0.1;
     Point.y = 10;
@@ -131,7 +131,7 @@ TEST(CheckPointWGS84UTooLowCorr)
 
 TEST(CheckPointWGS84VTooHighCorr)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 10;
     Point.y = 90.1;
@@ -142,7 +142,7 @@ TEST(CheckPointWGS84VTooHighCorr)
 
 TEST(CheckPointWGS84VTooLowCorr)
 {
-    asGeo geo(WGS84);
+    asGeo geo;
     Coo Point;
     Point.x = 10;
     Point.y = -90.1;
@@ -151,99 +151,4 @@ TEST(CheckPointWGS84VTooLowCorr)
     CHECK_CLOSE(-89.9, Point.y, 0.000001);
 }
 
-TEST(ProjTransformWGS84toCH1903)
-{
-    asGeo geo(WGS84);
-    Coo Point;
-    Point.x = 7.19;
-    Point.y = 46.06;
-    // http://topo.epfl.ch/misc/transcoco/transcoco_compute.php
-    Coo Result = geo.ProjTransform(CH1903, Point);
-    CHECK_CLOSE(580758.18, Result.x, 1);
-    CHECK_CLOSE(100972.32, Result.y, 1);
-}
-
-TEST(ProjTransformWGS84toCH1903p)
-{
-    asGeo geo(WGS84);
-    Coo Point;
-    Point.x = 7.465270;
-    Point.y = 46.877096;
-    Coo Result = geo.ProjTransform(CH1903p, Point);
-    // http://en.wikipedia.org/wiki/Swiss_coordinate_system
-    CHECK_CLOSE(2602030.74, Result.x, 1);
-    CHECK_CLOSE(1191775.03, Result.y, 1);
-}
-
-TEST(ProjTransformCH1903toWGS84)
-{
-    asGeo geo(CH1903);
-    Coo Point;
-    Point.x = 603460;
-    Point.y = 201200;
-    Coo Result = geo.ProjTransform(WGS84, Point);
-    // http://topo.epfl.ch/misc/transcoco/transcoco_compute.php
-    CHECK_CLOSE(7.484091, Result.x, 0.001);
-    CHECK_CLOSE(46.961871, Result.y, 0.001);
-}
-
-TEST(ProjTransformCH1903toCH1903p)
-{
-    asGeo geo(CH1903);
-    Coo Point;
-    Point.x = 602030.68;
-    Point.y = 191775.03;
-    Coo Result = geo.ProjTransform(CH1903p, Point);
-    // http://en.wikipedia.org/wiki/Swiss_coordinate_system
-    CHECK_CLOSE(2602030.74, Result.x, 1);
-    CHECK_CLOSE(1191775.03, Result.y, 1);
-}
-
-TEST(ProjTransformCH1903toWGS84High)
-{
-    asGeo geo(CH1903);
-    Coo Point;
-    Point.x = 803460;
-    Point.y = 291200;
-    Coo Result = geo.ProjTransform(WGS84, Point);
-    // http://topo.epfl.ch/misc/transcoco/transcoco_compute.php
-    CHECK_CLOSE(10.151699, Result.x, 0.001);
-    CHECK_CLOSE(47.739712, Result.y, 0.001);
-}
-
-TEST(ProjTransformCH1903ptoWGS84)
-{
-    asGeo geo(CH1903p);
-    Coo Point;
-    Point.x = 2602030.74;
-    Point.y = 1191775.03;
-    Coo Result = geo.ProjTransform(WGS84, Point);
-    // http://en.wikipedia.org/wiki/Swiss_coordinate_system
-    CHECK_CLOSE(7.465270, Result.x, 0.001);
-    CHECK_CLOSE(46.877096, Result.y, 0.001);
-}
-
-TEST(ProjTransformCH1903ptoCH1903)
-{
-    asGeo geo(CH1903p);
-    Coo Point;
-    Point.x = 2602030.74;
-    Point.y = 1191775.03;
-    Coo Result = geo.ProjTransform(CH1903, Point);
-    // http://en.wikipedia.org/wiki/Swiss_coordinate_system
-    CHECK_CLOSE(602030.68, Result.x, 1);
-    CHECK_CLOSE(191775.03, Result.y, 1);
-}
-
-TEST(ProjTransformCH1903ptoCH1903pSame)
-{
-    asGeo geo(CH1903p);
-    Coo Point;
-    Point.x = 2803460;
-    Point.y = 1291200;
-    Coo Result = geo.ProjTransform(CH1903p, Point);
-    CHECK_CLOSE(2803460, Result.x, 1);
-    CHECK_CLOSE(1291200, Result.y, 1);
-}
-*/
 }
