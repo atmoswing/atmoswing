cc436ae9cdba31833338808f8c1a550fe5e4c4f6
Pascal Horton <pascalhorton@users.noreply.github.com>
2016-03-09 17:23:47 +0100

    Update of the files header.

--- src/shared_base/core/asDataPredictorRealtime.h
+++ src/shared_base/core/asDataPredictorRealtime.h
@@ -15,14 +15,15 @@
  * by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
  *
- * The Original Software is AtmoSwing. The Initial Developer of the
- * Original Software is Pascal Horton of the University of Lausanne.
+ * The Original Software is AtmoSwing.
+ * The Original Software was developed at the University of Lausanne.
  * All Rights Reserved.
  *
  */
 
 /*
- * Portions Copyright 2008-2013 University of Lausanne.
+ * Portions Copyright 2008-2013 Pascal Horton, University of Lausanne.
+ * Portions Copyright 2013-2015 Pascal Horton, Terranum.
  */
 
 #ifndef ASDATAPREDICTORREALTIME_H
