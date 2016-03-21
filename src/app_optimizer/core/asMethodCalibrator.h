2f79a2b77a228e1c65a7f7497bc9a4b410f53af5
Pascal Horton <horton.pascal@gmail.com>
2016-03-21 11:36:36 +0100

    Mirroring changes from the optimizer.

--- src/app_optimizer/core/asMethodCalibrator.h
+++ src/app_optimizer/core/asMethodCalibrator.h
@@ -50,38 +50,66 @@
 #include <asForecastScore.h>
 
 
-class asMethodCalibrator: public asMethodStandard
+class asMethodCalibrator : public asMethodStandard
 {
 public:
     asMethodCalibrator();
+
     virtual ~asMethodCalibrator();
 
     bool GetAnalogsDates(asResultsAnalogsDates &results, asParametersScoring &params, int i_step, bool &containsNaNs);
-    bool GetAnalogsSubDates(asResultsAnalogsDates &results, asParametersScoring &params, asResultsAnalogsDates &anaDates, int i_step, bool &containsNaNs);
-    bool GetAnalogsValues(asResultsAnalogsValues &results, asParametersScoring &params, asResultsAnalogsDates &anaDates, int i_step);
-    bool GetAnalogsForecastScores(asResultsAnalogsForecastScores &results, asParametersScoring &params, asResultsAnalogsValues &anaValues, int i_step);
-    bool GetAnalogsForecastScoreFinal(asResultsAnalogsForecastScoreFinal &results, asParametersScoring &params, asResultsAnalogsForecastScores &anaScores, int i_step);
-    bool SubProcessAnalogsNumber(asParametersCalibration &params, asResultsAnalogsDates &anaDatesPrevious, int i_step = 0);
 
-    void Cleanup(std::vector < asDataPredictorArchive* > predictorsPreprocess);
-    void Cleanup(std::vector < asDataPredictor* > predictors);
-    void Cleanup(std::vector < asPredictorCriteria* > criteria);
+    bool GetAnalogsSubDates(asResultsAnalogsDates &results, asParametersScoring &params,
+                            asResultsAnalogsDates &anaDates, int i_step, bool &containsNaNs);
+
+    bool GetAnalogsValues(asResultsAnalogsValues &results, asParametersScoring &params, asResultsAnalogsDates &anaDates,
+                          int i_step);
+
+    bool GetAnalogsForecastScores(asResultsAnalogsForecastScores &results, asParametersScoring &params,
+                                  asResultsAnalogsValues &anaValues, int i_step);
+
+    bool GetAnalogsForecastScoreFinal(asResultsAnalogsForecastScoreFinal &results, asParametersScoring &params,
+                                      asResultsAnalogsForecastScores &anaScores, int i_step);
+
+    bool SubProcessAnalogsNumber(asParametersCalibration &params, asResultsAnalogsDates &anaDatesPrevious,
+                                 int i_step = 0);
+
+    bool PreloadDataWithoutPreprocessing(asParametersScoring &params, int i_step, int i_ptor, int i_dat);
+
+    bool PreloadDataWithPreprocessing(asParametersScoring &params, int i_step, int i_ptor);
+
+    void Cleanup(std::vector<asDataPredictorArchive *> predictorsPreprocess);
+
+    void Cleanup(std::vector<asDataPredictor *> predictors);
+
+    void Cleanup(std::vector<asPredictorCriteria *> criteria);
 
     void DeletePreloadedData();
+
     void ClearAll();
+
     void ClearTemp();
-    void ClearScores();
+
     void PushBackBestTemp();
+
     void RemoveNaNsInTemp();
+
     void KeepBestTemp();
+
     void PushBackFirstTemp();
+
     void KeepFirstTemp();
-    void SortScoresAndParameters();
+
     void SortScoresAndParametersTemp();
+
     bool PushBackInTempIfBetter(asParametersCalibration &params, asResultsAnalogsForecastScoreFinal &scoreFinal);
+
     bool KeepIfBetter(asParametersCalibration &params, asResultsAnalogsForecastScoreFinal &scoreFinal);
+
     bool SetSelectedParameters(asResultsParametersArray &results);
+
     bool SetBestParameters(asResultsParametersArray &results);
+
     wxString GetPredictandStationIdsList(VectorInt &stationIds);
 
     bool Manager();
@@ -108,29 +136,55 @@ public:
         m_scoreClimatology = val;
     }
 
-
 protected:
+
     VectorFloat m_scoresCalib;
     VectorFloat m_scoresCalibTemp;
     Order m_scoreOrder;
     float m_scoreValid;
     VectorFloat m_scoreClimatology;
-    std::vector <asParametersCalibration> m_parameters;
-    std::vector <asParametersCalibration> m_parametersTemp;
+    std::vector<asParametersCalibration> m_parameters;
+    std::vector<asParametersCalibration> m_parametersTemp;
     asParametersCalibration m_originalParams;
     bool m_preloaded;
     bool m_validationMode;
-    std::vector < std::vector < std::vector < std::vector < asDataPredictorArchive* > > > > m_preloadedArchive;
-    VVectorBool m_preloadedArchivePointerCopy;
 
     virtual bool Calibrate(asParametersCalibration &params) = 0;
+
     bool PreloadData(asParametersScoring &params);
-    bool LoadData(std::vector < asDataPredictor* > &predictors, asParametersScoring &params, int i_step, double timeStartData, double timeEndData);
+
+    bool LoadData(std::vector<asDataPredictor *> &predictors, asParametersScoring &params, int i_step,
+                  double timeStartData, double timeEndData);
+
     VArray1DFloat GetClimatologyData(asParametersScoring &params);
 
 private:
 
+    std::vector<std::vector<std::vector<std::vector<std::vector<asDataPredictorArchive *> > > > > m_preloadedArchive;
+    std::vector<VVectorBool > m_preloadedArchivePointerCopy;
 
+    bool PointersShared(asParametersScoring &params, int i_step, int i_ptor, int i_dat);
+
+    double GetTimeStartCalibration(asParametersScoring &params) const;
+
+    double GetTimeStartArchive(asParametersScoring &params) const;
+
+    double GetTimeEndCalibration(asParametersScoring &params) const;
+
+    double GetTimeEndArchive(asParametersScoring &params) const;
+
+    void InitializePreloadedDataContainer(asParametersScoring &params);
+
+    void LoadScoreOrder(asParametersCalibration &params);
+
+    bool ExtractPreloadedData(std::vector<asDataPredictor *> &predictors, asParametersScoring &params, int i_step,
+                              int i_ptor);
+
+    bool ExtractDataWithoutPreprocessing(std::vector<asDataPredictor *> &predictors, asParametersScoring &params, int i_step,
+                                      int i_ptor, double timeStartData, double timeEndData);
+
+    bool ExtractDataWithPreprocessing(std::vector<asDataPredictor *> &predictors, asParametersScoring &params, int i_step,
+                                      int i_ptor, double timeStartData, double timeEndData);
 };
 
 #endif // ASMETHODCALIBRATOR_H
--- src/shared_base/core/asLog.cpp
+++ src/shared_base/core/asLog.cpp
@@ -8,9 +8,9 @@
  * You can read the License at http://opensource.org/licenses/CDDL-1.0
  * See the License for the specific language governing permissions
  * and limitations under the License.
- * 
- * When distributing Covered Code, include this CDDL Header Notice in 
- * each file and include the License file (licence.txt). If applicable, 
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in
+ * each file and include the License file (licence.txt). If applicable,
  * add the following below this CDDL Header, with the fields enclosed
  * by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
@@ -18,14 +18,14 @@
  * The Original Software is AtmoSwing.
  * The Original Software was developed at the University of Lausanne.
  * All Rights Reserved.
- * 
+ *
  */
 
 /*
  * Portions Copyright 2008-2013 Pascal Horton, University of Lausanne.
  * Portions Copyright 2013-2015 Pascal Horton, Terranum.
  */
- 
+
 #include "asLog.h"
 
 #include <wx/ffile.h>
@@ -125,14 +125,16 @@ void asLog::Resume()
 void asLog::Flush()
 {
 #if wxUSE_GUI
-    if (m_logChain) {
-        m_logChain->Flush();
-    }
-    else if (m_logFile) {
+	if (m_logChain) {
+		m_logChain->Flush();
+	}
+	else if (m_logFile) {
+		m_logFile->Flush();
+	}
+#else
+    if(m_logFile) {
         m_logFile->Flush();
     }
-#else
-	m_logFile->Flush();
 #endif
 }
 
