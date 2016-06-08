3dbfe05405baa437ce4ad4dee080bf256459ca55
Pascal Horton <pascalhorton@users.noreply.github.com>
2016-06-08 09:47:06 +0200

    Changing casing in structures.

--- src/app_optimizer/core/asMethodCalibratorEvaluateAllScores.cpp
+++ src/app_optimizer/core/asMethodCalibratorEvaluateAllScores.cpp
@@ -74,7 +74,7 @@ bool asMethodCalibratorEvaluateAllScores::Calibrate(asParametersCalibration &par
     for (int i_step = 0; i_step < params.GetStepsNb(); i_step++) {
         if (params.GetAnalogsNumberVector(i_step).size() > 1) {
             checkSizes = false;
-            errorField.Append(wxString::Format("AnalogsNumber (step %d), ", i_step));
+            errorField.Append(wxString::Format("analogsNumber (step %d), ", i_step));
         }
         for (int i_predictor = 0; i_predictor < params.GetPredictorsNb(i_step); i_predictor++) {
             if (params.NeedsPreprocessing(i_step, i_predictor)) {
@@ -82,7 +82,7 @@ bool asMethodCalibratorEvaluateAllScores::Calibrate(asParametersCalibration &par
                     if (params.GetPreprocessDataIdVector(i_step, i_predictor, i_pre).size() > 1) {
                         checkSizes = false;
                         errorField.Append(
-                                wxString::Format("PreprocessDataId (step %d, predictor %d, preprocess %d), ", i_step,
+                                wxString::Format("preprocessDataId (step %d, predictor %d, preprocess %d), ", i_step,
                                                  i_predictor, i_pre));
                     }
                     if (params.GetPreprocessLevelVector(i_step, i_predictor, i_pre).size() > 1) {
@@ -94,7 +94,7 @@ bool asMethodCalibratorEvaluateAllScores::Calibrate(asParametersCalibration &par
                     if (params.GetPreprocessTimeHoursVector(i_step, i_predictor, i_pre).size() > 1) {
                         checkSizes = false;
                         errorField.Append(
-                                wxString::Format("PreprocessTimeHours (step %d, predictor %d, preprocess %d), ", i_step,
+                                wxString::Format("preprocessTimeHours (step %d, predictor %d, preprocess %d), ", i_step,
                                                  i_predictor, i_pre));
                     }
                 }
