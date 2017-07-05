3fc1bd857901e9dd6d8e642f33fc7c3fecf3c92e
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-07-05 17:00:10 +0200

    Working on libraries auto management.
    Implementing Hunter and external projects.

--- build/cmake/ASgetLibraries.cmake
+++ build/cmake/ASgetLibraries.cmake
@@ -2,131 +2,184 @@
 include(ExternalProject)
 set(EXTERNAL_DIR ${CMAKE_BINARY_DIR}/external)
 
+
 # Own libraries
 add_library(asbase STATIC ${src_shared_base})
 if (BUILD_FORECASTER OR BUILD_OPTIMIZER)
     add_library(asprocessing STATIC ${src_shared_processing})
 endif (BUILD_FORECASTER OR BUILD_OPTIMIZER)
 
+# wxplotctrl
 if (BUILD_VIEWER)
     add_library(wxplotctrl STATIC ${src_lib_wxplotctrl})
 endif (BUILD_VIEWER)
-
-# WxWidgets (adv lib nedded for the caldendar widget)
-mark_as_advanced(wxWidgets_wxrc_EXECUTABLE)
-mark_as_advanced(wxWidgets_with_GUI)
-if (USE_MSYS2)
-    set(wxWidgets_CONFIG_OPTIONS --prefix=${MINGW_PATH})
-endif ()
-if (USE_GUI)
-    set(wxWidgets_with_GUI TRUE)
-    find_package(wxWidgets COMPONENTS core base adv xml REQUIRED)
-else (USE_GUI)
-    set(wxWidgets_with_GUI FALSE)
-    find_package(wxWidgets COMPONENTS base xml REQUIRED)
-endif (USE_GUI)
-include("${wxWidgets_USE_FILE}")
-include_directories(${wxWidgets_INCLUDE_DIRS})
-
-# NetCDF (has to be before GDAL)
-mark_as_advanced(CLEAR NetCDF_INCLUDE_DIRECTORIES)
-mark_as_advanced(CLEAR NetCDF_C_LIBRARY)
-find_package(NetCDF REQUIRED)
-include_directories(${NetCDF_INCLUDE_DIRECTORIES})
-
-# Jasper
-if (PATH_JASPER)
-    find_package(Jasper REQUIRED PATHS PATH_JASPER)
-    include_directories(${JASPER_INCLUDE_DIR})
-    include_directories(${JPEG_INCLUDE_DIR})
-else (PATH_JASPER)
-    ExternalProject_Add(jasper
-            URL "http://www.ece.uvic.ca/~mdadams/jasper/software/jasper-1.900.1.zip"
-            #PATCH_COMMAND patch < http://www.linuxfromscratch.org/patches/blfs/svn/jasper-1.900.1-security_fixes-1.patch
-            CONFIGURE_COMMAND ./configure --prefix=${EXTERNAL_DIR}
-            BUILD_COMMAND make -j4
-            INSTALL_COMMAND make install
-            )
-endif (PATH_JASPER)
-
-
-# PNG
-find_package(PNG REQUIRED)
-include_directories(${PNG_INCLUDE_DIRS})
 
 # Grib2c
 add_library(g2clib STATIC ${src_lib_g2clib})
 include_directories("src/shared_base/libs/g2clib")
 
-# libcURL
-if (BUILD_FORECASTER OR BUILD_VIEWER)
-    if (PATH_CURL)
+
+# Donwload libraries
+if (DOWNLOAD_LIBRARIES)
+
+    # WxWidgets
+    hunter_add_package(wxWidgets)
+    if (USE_MSYS2)
+        set(wxWidgets_CONFIG_OPTIONS --prefix=${MINGW_PATH})
+    endif (USE_MSYS2)
+    if (USE_GUI)
+        set(wxWidgets_with_GUI TRUE)
+        find_package(wxWidgets REQUIRED core base adv xml net)
+    else (USE_GUI)
+        set(wxWidgets_with_GUI FALSE)
+        find_package(wxWidgets REQUIRED base xml net)
+    endif (USE_GUI)
+    include(${wxWidgets_USE_FILE})
+    include_directories(${WXWIDGETS_ROOT}/include)
+
+
+
+    # OpenSSL
+    #hunter_add_package(OpenSSL)
+    #find_package(OpenSSL REQUIRED)
+    #include_directories("${OPENSSL_INCLUDE_DIR}")
+
+    # libcURL
+    #hunter_add_package(CURL)
+    #find_package(CURL REQUIRED)
+    #set(CURL_LIBRARIES CURL::libcurl)
+    #include_directories(${CURL_INCLUDE_DIRS})
+
+    # libcURL
+    configure_file(build/cmake/CMakeListsCurl.txt.in ${CMAKE_BINARY_DIR}/libcurl/CMakeLists.txt)
+    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libcurl" )
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libcurl" )
+    find_package(OpenSSL REQUIRED)
+    find_package(CURL REQUIRED PATHS ${EXTERNAL_DIR} NO_SYSTEM_ENVIRONMENT_PATH)
+
+    # PNG
+    hunter_add_package(PNG)
+    find_package(PNG REQUIRED)
+
+    # Jpg
+    hunter_add_package(Jpeg)
+    find_package(JPEG REQUIRED)
+
+    # HDF5
+    hunter_add_package(hdf5)
+    find_package(ZLIB REQUIRED)
+    find_package(szip REQUIRED)
+    find_package(hdf5 REQUIRED)
+
+    # NetCDF
+    configure_file(build/cmake/CMakeListsNetcdf.txt.in ${CMAKE_BINARY_DIR}/libnetcdf/CMakeLists.txt)
+    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libnetcdf" )
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libnetcdf" )
+    find_package(netCDF REQUIRED PATHS ${EXTERNAL_DIR} NO_SYSTEM_ENVIRONMENT_PATH)
+
+    # Jasper
+    configure_file(build/cmake/CMakeListsJasper.txt.in ${CMAKE_BINARY_DIR}/libjasper/CMakeLists.txt)
+    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libjasper" )
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libjasper" )
+    find_library(JASPER_LIBRARIES NAMES jasper libjasper PATHS ${EXTERNAL_DIR}/lib NO_DEFAULT_PATH)
+
+    # GDAL
+    if (BUILD_VIEWER)
+        configure_file(build/cmake/CMakeListsGdal.txt.in ${CMAKE_BINARY_DIR}/libgdal/CMakeLists.txt)
+        execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+                WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libgdal" )
+        execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+                WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/libgdal" )
+        find_package(GDAL REQUIRED PATHS ${EXTERNAL_DIR} NO_DEFAULT_PATH)
+    else (BUILD_VIEWER)
+        # unset for wxhgversion
+        unset(GDAL_INCLUDE_DIR CACHE)
+        unset(GDAL_LIBRARY CACHE)
+    endif (BUILD_VIEWER)
+
+else(DOWNLOAD_LIBRARIES)
+
+    # WxWidgets (adv lib nedded for the caldendar widget)
+    mark_as_advanced(wxWidgets_wxrc_EXECUTABLE)
+    mark_as_advanced(wxWidgets_with_GUI)
+    if (USE_MSYS2)
+        set(wxWidgets_CONFIG_OPTIONS --prefix=${MINGW_PATH})
+    endif (USE_MSYS2)
+    if (USE_GUI)
+        set(wxWidgets_with_GUI TRUE)
+        find_package(wxWidgets REQUIRED core base adv xml net)
+    else (USE_GUI)
+        set(wxWidgets_with_GUI FALSE)
+        find_package(wxWidgets REQUIRED base xml net)
+    endif (USE_GUI)
+    include("${wxWidgets_USE_FILE}")
+    include_directories(${wxWidgets_INCLUDE_DIRS})
+
+    # libcURL
+    if (BUILD_FORECASTER OR BUILD_VIEWER)
         mark_as_advanced(CLEAR CURL_INCLUDE_DIR)
         mark_as_advanced(CLEAR CURL_LIBRARY)
-        find_package(CURL REQUIRED PATHS PATH_CURL)
+        find_package(OpenSSL REQUIRED)
+        find_package(CURL REQUIRED)
         include_directories(${CURL_INCLUDE_DIRS})
-    else(PATH_CURL)
-        ExternalProject_Add(libcurl
-                GIT_REPOSITORY https://github.com/curl/curl
-                GIT_TAG ${CURL_GIT_TAG}
-                CMAKE_ARGS -DHTTP_ONLY=ON -DCURL_STATICLIB=OFF -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=0 -DCMAKE_INSTALL_PREFIX=${EXTERNAL_DIR}
-                )
-        set(CURL_INCLUDE_DIR ${EXTERNAL_DIR}/include)
-    endif(PATH_CURL)
-else (BUILD_FORECASTER OR BUILD_VIEWER)
-    # unset for wxhgversion
-    unset(CURL_INCLUDE_DIR CACHE)
-    unset(CURL_LIBRARY CACHE)
-endif (BUILD_FORECASTER OR BUILD_VIEWER)
+    else (BUILD_FORECASTER OR BUILD_VIEWER)
+        # unset for wxhgversion
+        unset(CURL_INCLUDE_DIR CACHE)
+        unset(CURL_LIBRARY CACHE)
+    endif (BUILD_FORECASTER OR BUILD_VIEWER)
 
-# GDAL
-if (BUILD_VIEWER)
-    if (PATH_GDAL)
-        find_package(GDAL REQUIRED PATHS PATH_GDAL)
+    # PNG
+    find_package(PNG REQUIRED)
+    include_directories(${PNG_INCLUDE_DIRS})
+
+    # NetCDF (has to be before GDAL)
+    mark_as_advanced(CLEAR NetCDF_INCLUDE_DIRECTORIES)
+    mark_as_advanced(CLEAR NetCDF_C_LIBRARY)
+    find_package(NetCDF REQUIRED)
+    include_directories(${NetCDF_INCLUDE_DIRECTORIES})
+
+    # Jasper
+    find_package(Jasper REQUIRED)
+    include_directories(${JASPER_INCLUDE_DIR})
+    include_directories(${JPEG_INCLUDE_DIR})
+
+    # GDAL
+    if (BUILD_VIEWER)
+        find_package(GDAL REQUIRED)
         include_directories(${GDAL_INCLUDE_DIRS})
-    else(PATH_GDAL)
-        if (PATH_CURL)
-            set(WITH_CURL_PATH PATH_CURL)
-        else (PATH_CURL)
-            set(WITH_CURL_PATH ${EXTERNAL_DIR})
-        endif (PATH_CURL)
-
-        if (MINGW OR MSYS OR UNIX)
-
-            ExternalProject_Add(gdal
-                    DEPENDS curl jasper
-                    URL "http://download.osgeo.org/gdal/CURRENT/gdal-${GDAL_VERSION}.tar.gz"
-                    CONFIGURE_COMMAND ./configure --prefix=${EXTERNAL_DIR} --with-jasper=${EXTERNAL_DIR} --with-curl=${WITH_CURL_PATH}
-                    BUILD_COMMAND make -j4
-                    INSTALL_COMMAND make install
-                    )
-        elseif(WIN32)
+    else (BUILD_VIEWER)
+        # unset for wxhgversion
+        unset(GDAL_INCLUDE_DIR CACHE)
+        unset(GDAL_LIBRARY CACHE)
+    endif (BUILD_VIEWER)
 
 
-        endif()
-    endif(PATH_GDAL)
-else (BUILD_VIEWER)
-    # unset for wxhgversion
-    unset(GDAL_INCLUDE_DIR CACHE)
-    unset(GDAL_LIBRARY CACHE)
-endif (BUILD_VIEWER)
+endif(DOWNLOAD_LIBRARIES)
 
-# Eigen
-ExternalProject_Add(eigen
-        URL "http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.gz"
-        UPDATE_COMMAND ""
-        CONFIGURE_COMMAND ""
-        BUILD_COMMAND ""
-        INSTALL_COMMAND
-        ${CMAKE_COMMAND} -E copy_directory
-        ${CMAKE_BINARY_DIR}/eigen-prefix/src/eigen/Eigen
-        ${EXTERNAL_DIR}/include/Eigen
-        )
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # vroomgis
 if (BUILD_VIEWER)
     ExternalProject_Add(vroomgis
-            URL "https://bitbucket.org/terranum/vroomgis/get/tip.tar.gz"
+            URL https://bitbucket.org/terranum/vroomgis/get/tip.tar.gz
             SOURCE_SUBDIR vroomgis
             CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_DIR} -DVROOMGIS_PATH=vroomgis/src
             )
@@ -148,6 +201,26 @@ if (USE_GUI)
 else (USE_GUI)
     set(USE_WXHGVERSION 0)
 endif (USE_GUI)
+
+
+
+
+
+
+
+
+
+# Eigen
+ExternalProject_Add(eigen
+        URL http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.gz
+        UPDATE_COMMAND ""
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND
+        ${CMAKE_COMMAND} -E copy_directory
+        ${CMAKE_BINARY_DIR}/eigen-prefix/src/eigen/Eigen
+        ${EXTERNAL_DIR}/include/Eigen
+        )
 
 # CUDA
 if (USE_CUDA)
