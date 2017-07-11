56e01c55a8a792d4d365bcd42bfea2d3fc4ee4b4
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-07-11 14:42:24 +0200

    Compiling GDAL on Travis.

---
+++ ci/build-gdal.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Build GDAL2
+if [ ! "$(ls -A ${HOME}/.libs/gdal)" ]; then
+  wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz" > /dev/null
+  tar -xzf gdal.tar.gz
+  cd gdal-2.1.3
+  ./configure --prefix=${HOME}/.libs/gdal --with-static-proj4=/usr --with-sqlite3=no --with-python=no --with-pg=no --with-grass=no --with-jasper=/usr --with-curl=/usr --with-jpeg=internal --with-png=internal --silent
+  make -j$(nproc) > /dev/null
+  make install > /dev/null
+  cd ..
+else 
+  printf 'GDAL will not be built (%s/.libs/gdal not empty).\n' "$HOME"
+fi
