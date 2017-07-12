916344e96a51f474dd61f0e0f224871df39451d7
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-07-12 13:34:38 +0200

    Update run-coverage.sh

--- ci/travis/run-coverage.sh
+++ ci/travis/run-coverage.sh
@@ -2,7 +2,7 @@
 
 cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_VIEWER=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_CODECOV=1 -DGDAL_ROOT=$HOME/.libs/gdal
 make -j$(nproc) atmoswing-coverage > /dev/null
-lcov --directory . --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
-lcov --list coverage.info #debug info
+lcov --directory . --capture --output-file coverage.info --quiet
+lcov --remove coverage.info '*/tests/*' 'bin/*' '*/libs/*' '*/.libs/*' '*/include/*' '/usr/*' --output-file coverage.info
+lcov --list coverage.info --quiet
 curl -s https://codecov.io/bash || echo "Codecov did not collect coverage reports"
