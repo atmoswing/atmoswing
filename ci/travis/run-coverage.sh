d1b8a768697dba5d02f470eec3a50617925b0677
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-07-12 10:53:36 +0200

    Adding Coverity scan to Travis builds.

--- ci/travis/run-coverage.sh
+++ ci/travis/run-coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_VIEWER=1 -DCMAKE_BUILD_TYPE=Debug -DUSE_CODECOV=1 -DGDAL_ROOT=$HOME/.libs/gdal
+cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_VIEWER=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_CODECOV=1 -DGDAL_ROOT=$HOME/.libs/gdal
 make -j$(nproc) atmoswing-coverage > /dev/null
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
