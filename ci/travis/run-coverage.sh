d372ad8eeb48ffc15c51f8a1cccceb2802a2f9cc
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-07-12 13:22:56 +0200

    Update run-coverage.sh

--- ci/travis/run-coverage.sh
+++ ci/travis/run-coverage.sh
@@ -5,4 +5,4 @@ make -j$(nproc) atmoswing-coverage > /dev/null
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
 lcov --list coverage.info #debug info
-bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+curl -s https://codecov.io/bash || echo "Codecov did not collect coverage reports"
