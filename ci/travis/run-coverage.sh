66a8cd582f422dd66194e5c69ab43453f98e56ea
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-07-14 09:48:07 +0200

    Removing tests directory from code coverage.

--- ci/travis/run-coverage.sh
+++ ci/travis/run-coverage.sh
@@ -2,6 +2,7 @@
 
 cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_VIEWER=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_CODECOV=1 -DGDAL_ROOT=$HOME/.libs/gdal
 make -j$(nproc) atmoswing-coverage > /dev/null
-lcov --directory . --capture --output-file coverage.info --quiet > /dev/null
-lcov --remove coverage.info '/usr/*' 'tests/*' 'tests/src/*' '*Test.cpp' 'bin/*' '*/libs/*' '*/.libs/*' '*/include/*' --output-file coverage.info --quiet > /dev/null
+lcov --directory . --capture --output-file coverage.info &> /dev/null
+lcov --remove coverage.info '/usr/*' '*/external/*' '*/tests/*' --output-file coverage.info --quiet
+lcov --list coverage.info
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports" 
