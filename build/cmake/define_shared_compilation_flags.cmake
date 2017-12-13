323d46fc1e0694ec3429356d69ff19636612fd50
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-12-13 12:05:40 +0100

    Better handling of compilation flags in CMake.

--- build/cmake/define_shared_compilation_flags.cmake
+++ build/cmake/define_shared_compilation_flags.cmake
@@ -5,7 +5,7 @@ set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_DEBUG -DwxDEBUG_LEVEL=1 -
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -DwxDEBUG_LEVEL=0 -DNDEBUG ")
 
 if (MINGW OR MSYS OR UNIX AND NOT APPLE)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -Wall -fno-strict-aliasing -Wno-sign-compare -Wno-attributes")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -fno-strict-aliasing -Wno-sign-compare -Wno-attributes")
     set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0")
     set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -fno-omit-frame-pointer ")
     if (USE_WXHGVERSION)
@@ -30,6 +30,10 @@ endif ()
 add_definitions(-DUSE_JPEG2000)
 add_definitions(-DEIGEN_NO_DEBUG)
 
+if (UNIX)
+    add_definitions(-std=c++11)
+endif (UNIX)
+
 if (WIN32)
     add_definitions(-D_CRT_SECURE_NO_WARNINGS)
     add_definitions(-D_CRTDBG_MAP_ALLOC)
@@ -39,6 +43,3 @@ if (USE_VLD)
     add_definitions(-DUSE_VLD)
 endif (USE_VLD)
 
-
-# Specific definitions
-set_target_properties(g2clib PROPERTIES COMPILE_DEFINITIONS "USE_JPEG2000; USE_PNG")
