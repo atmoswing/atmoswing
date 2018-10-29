3de98a70bc6a895c78166edf76807ee8c4c63c2b
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-10-29 16:00:54 +0100

    Using WiX to build MSI (fixes #188)

--- ci/appveyor/libs-server-install.ps1
+++ ci/appveyor/libs-server-install.ps1
@@ -17,6 +17,21 @@ if ($APPVEYOR) {
 }
 Write-Host "`n$(cmake --version)" -ForegroundColor Yellow
 
+# Install WIX
+if ($APPVEYOR) {
+  Write-Host "`nInstalling WIX" -ForegroundColor Yellow
+  cd $TMP_DIR
+  $WIX_URL="https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311-binaries.zip"
+  if ($APPVEYOR) {
+    appveyor DownloadFile $WIX_URL -FileName wix.zip > $null
+  } else {
+    Invoke-WebRequest -Uri $WIX_URL -OutFile wix.zip
+  }
+  7z x wix.zip -o"$TMP_DIR" > $null
+  move "$TMP_DIR\wix311-binaries\*" "$WIX_DIR"
+  $env:Path += ";$WIX_DIR\bin"
+}
+
 # Install wxWidgets
 if(!(Test-Path -Path "$LIB_DIR\include\wx") -Or $REBUILD_WX) {
   Init-Build "wxwidgets"
