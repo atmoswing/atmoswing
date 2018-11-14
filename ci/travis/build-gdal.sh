4d8a2b5e79a8e1a60d0819f2d8acf4c4e340adda
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-11-14 23:13:47 +0100

    Changes in Travis CI.

--- ci/travis/build-gdal.sh
+++ ci/travis/build-gdal.sh
@@ -5,7 +5,7 @@ if [ ! "$(ls -A ${HOME}/.libs/gdal)" ]; then
   wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz" > /dev/null
   tar -xzf gdal.tar.gz
   cd gdal-2.1.3
-  ./configure --prefix=${HOME}/.libs/gdal --with-static-proj4=/usr --with-sqlite3=no --with-python=no --with-pg=no --with-grass=no --with-jasper=/usr --with-curl=/usr --with-jpeg=internal --with-png=internal --silent
+  ./configure --prefix=${HOME}/.libs --with-static-proj4=/usr --with-sqlite3=no --with-python=no --with-pg=no --with-grass=no --with-jasper=/usr --with-curl=/usr --with-jpeg=internal --with-png=internal --silent
   make -j6 > /dev/null
   make install > /dev/null
   cd ..
---
+++ ci/travis/dependencies-osx.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+brew tap homebrew/science
+brew install proj
+brew install netcdf
+
+# Build libraries
+chmod +x ci/travis/build-wxwidgets-osx.sh
+ci/travis/build-wxwidgets-osx.sh
+chmod +x ci/travis/build-gdal-osx.sh
+ci/travis/build-gdal-osx.sh
+
+# Changing permissions of Homebrew libraries
+sudo chmod -R 777 /usr/local/Cellar
