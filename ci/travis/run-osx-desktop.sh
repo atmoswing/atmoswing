9b9422635f0132afb8876cca5004daf5b9380f72
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-11-16 11:25:30 +0100

    Fixing openssl on Travis osx.

--- ci/travis/run-osx-desktop.sh
+++ ci/travis/run-osx-desktop.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+export OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
+
 cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_DOWNSCALER=1 -DBUILD_VIEWER=1 -DUSE_GUI=1 -DCMAKE_BUILD_TYPE=Release -DGDAL_ROOT=$HOME/.libs -DCMAKE_PREFIX_PATH=$HOME/.libs
 make -j6
