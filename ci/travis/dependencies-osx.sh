9f63fc94d30cab27f538b2a8cc48c1f8138eb45b
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-11-16 13:48:31 +0100

    Trying to use the installed openssl version on Travis osx.

--- ci/travis/dependencies-osx.sh
+++ ci/travis/dependencies-osx.sh
@@ -3,10 +3,6 @@
 brew install proj
 brew install jasper
 brew install netcdf
-brew upgrade openssl
-
-brew link openssl --force
-whereis openssl
 
 # Build libraries
 chmod +x ci/travis/build-wxwidgets-osx.sh
