af3f188b25f85d25fa0da9b11115b63acdabbaa6
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-11-18 14:20:51 +0100

    Changing the build order on Travis.

--- ci/travis/dependencies-osx.sh
+++ ci/travis/dependencies-osx.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
 brew install proj
-brew install jasper
-brew install netcdf
 
 # Build libraries
 chmod +x ci/travis/build-wxwidgets-osx.sh
 ci/travis/build-wxwidgets-osx.sh
 chmod +x ci/travis/build-gdal-osx.sh
 ci/travis/build-gdal-osx.sh
+
+brew install jasper
+brew install netcdf
 
 # Changing permissions of Homebrew libraries
 sudo chmod -R 777 /usr/local/Cellar
