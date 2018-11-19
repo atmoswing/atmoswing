1802f596750c1045ad15e5653f66e74f1c17bcf3
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-11-19 10:53:30 +0100

    Debugging Travis osx build.

--- ci/travis/build-wxwidgets-osx.sh
+++ ci/travis/build-wxwidgets-osx.sh
@@ -4,9 +4,9 @@ REBUILD_WX=false
 
 # Build wxWidgets
 if [ ! "$(ls -A ${HOME}/.libs/include/wx-3.1)" ] || [ "$REBUILD_WX" = true ]; then
-  wget -q -O wxwidgets.tar.bz2 "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2" > /dev/null
+  wget -q -O wxwidgets.tar.bz2 "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.1/wxWidgets-3.1.1.tar.bz2" > /dev/null
   tar -xjf wxwidgets.tar.bz2
-  cd wxWidgets-3.1.0
+  cd wxWidgets-3.1.1
   export LDFLAGS="-stdlib=libc++"
   export OBJCXXFLAGS="-stdlib=libc++ -std=c++11"
   ./configure --prefix=${HOME}/.libs --enable-unicode --disable-shared --enable-mediactrl=no --enable-webview --enable-webview-webkit --silent --with-macosx-version-min=10.10
