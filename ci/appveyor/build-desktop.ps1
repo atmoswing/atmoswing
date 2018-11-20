c13fe8c57dfe2ded132cde144eab5fa53880b069
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-11-20 15:41:15 +0100

    Fixing find GDAL on AppVeyor.

--- ci/appveyor/build-desktop.ps1
+++ ci/appveyor/build-desktop.ps1
@@ -1,5 +1,5 @@
 echo Running cmake...
 mkdir bin
 cd bin
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_FORECASTER=ON -DBUILD_OPTIMIZER=ON -DBUILD_DOWNSCALER=ON -DBUILD_VIEWER=ON -DUSE_GUI=ON -DCMAKE_PREFIX_PATH="C:\projects\libs" -DwxWidgets_CONFIGURATION=mswu ..
+cmake -G "Visual Studio 15 2017 Win64" -DBUILD_FORECASTER=ON -DBUILD_OPTIMIZER=ON -DBUILD_DOWNSCALER=ON -DBUILD_VIEWER=ON -DUSE_GUI=ON -DGDAL_ROOT="C:\projects\libs" -DCMAKE_PREFIX_PATH="C:\projects\libs" -DwxWidgets_CONFIGURATION=mswu ..
 cmake --build . --config release
