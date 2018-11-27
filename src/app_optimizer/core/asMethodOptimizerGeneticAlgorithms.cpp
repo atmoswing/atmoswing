78da65109db3ee7cadbc26b2e683c1018b881932
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-11-27 09:58:48 +0100

    Fixing path of a resulting file.

--- src/app_optimizer/core/asMethodOptimizerGeneticAlgorithms.cpp
+++ src/app_optimizer/core/asMethodOptimizerGeneticAlgorithms.cpp
@@ -247,7 +247,7 @@ bool asMethodOptimizerGeneticAlgorithms::ManageOneRun()
     results_generations.Init(wxString::Format(_("station_%s_generations"),
                                               GetPredictandStationIdsList(stationId).c_str()));
     wxString resultsXmlFilePath = pConfig->Read("/Paths/ResultsDir", asConfig::GetDefaultUserWorkingDir());
-    resultsXmlFilePath.Append(wxString::Format("%s_station_%s_best_parameters.xml", time.c_str(),
+    resultsXmlFilePath.Append(wxString::Format("/%s_station_%s_best_parameters.xml", time.c_str(),
                                                GetPredictandStationIdsList(stationId).c_str()));
     int counterPrint = 0;
 
