508832d7de79ca9cf49fb2f50de491568e6bc932
Pascal Horton <pascal.horton@giub.unibe.ch>
2019-05-07 13:56:52 +0200

    Building Proj4 v.6 on Travis.

--- ci/travis/dependencies-linux.sh
+++ ci/travis/dependencies-linux.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 # Build libraries
+chmod +x ci/travis/build-proj.sh
+ci/travis/build-proj.sh
 chmod +x ci/travis/build-gdal.sh
 ci/travis/build-gdal.sh
 chmod +x ci/travis/build-eccodes.sh
