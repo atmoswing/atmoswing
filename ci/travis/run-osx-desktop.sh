0c26200d77f783a501a9cec2887c6d7e761022da
Pascal Horton <pascal.horton@giub.unibe.ch>
2019-05-10 09:17:11 +0200

    Debugging Travis osx builds.

--- ci/travis/run-osx-desktop.sh
+++ ci/travis/run-osx-desktop.sh
@@ -5,9 +5,5 @@ export OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
 cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_DOWNSCALER=1 -DBUILD_VIEWER=1 -DCREATE_INSTALLER=ON -DUSE_GUI=1 -DCMAKE_BUILD_TYPE=Release -DGDAL_ROOT=$HOME/.libs -DCMAKE_PREFIX_PATH=$HOME/.libs
 make -j6
 
-echo "-----------"
-pwd
-echo "-----------"
-find /Users/travis/ -name atmoswing-tests
-echo "-----------"
+cd tests
 ctest -V
--- ci/travis/run-osx-server.sh
+++ ci/travis/run-osx-server.sh
@@ -5,5 +5,9 @@ export OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
 cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_DOWNSCALER=1 -DBUILD_VIEWER=0 -DCREATE_INSTALLER=ON -DUSE_GUI=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$HOME/.libs
 make -j6
 
-cd tests
+echo "-----------"
+pwd
+echo "-----------"
+find /Users/travis/ -name atmoswing-tests
+echo "-----------"
 ctest -V
