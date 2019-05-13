886319ca4be60b516e9255755072bef5b106fe68
Pascal Horton <pascal.horton@giub.unibe.ch>
2019-05-13 17:07:04 +0200

    Disabling osx desktop build.

--- ci/travis/build-gdal-osx.sh
+++ ci/travis/build-gdal-osx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REBUILD_GDAL=true
+REBUILD_GDAL=false
 GDAL_VERSION=3.0.0
 
 # Build GDAL2
@@ -8,8 +8,8 @@ if [ ! "$(ls -A ${HOME}/.libs/include/gdal.h)" ] || [ "$REBUILD_GDAL" = true ]; 
   wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz" > /dev/null
   tar -xzf gdal.tar.gz
   cd gdal-${GDAL_VERSION}
-  ./configure --prefix=${HOME}/.libs --with-proj=/usr/local --with-sqlite3=no --with-python=no --with-pg=no --with-grass=no --with-jasper=/usr --with-curl=/usr --with-jpeg=internal --with-png=internal #--silent
-  make -j6 #> /dev/null
+  ./configure --prefix=${HOME}/.libs --with-proj=/usr/local --with-sqlite3=no --with-python=no --with-pg=no --with-grass=no --with-jasper=/usr --with-curl=/usr --with-jpeg=internal --with-png=internal --silent
+  make -j6 > /dev/null
   make install
   cd ..
   printf 'GDAL has been built.\n'
