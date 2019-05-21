f328fd7084941f213a96c2157254c00efc7d71b2
Pascal Horton <pascal.horton@giub.unibe.ch>
2019-05-21 14:40:46 +0200

    Improving Proj4 CMake find.

--- build/cmake/FindPROJ4.cmake
+++ build/cmake/FindPROJ4.cmake
@@ -5,9 +5,9 @@
 #  PROJ4_LIBRARIES   - List of libraries when using PROJ4
 #  PROJ4_FOUND       - True if PROJ4 was found
 
-if(PROJ4_INCLUDE_DIR)
-  set(PROJ4_FIND_QUIETLY TRUE)
-endif(PROJ4_INCLUDE_DIR)
+if (PROJ4_INCLUDE_DIR)
+    set(PROJ4_FIND_QUIETLY TRUE)
+endif (PROJ4_INCLUDE_DIR)
 
 find_path(PROJ4_INCLUDE_DIR
         NAMES proj.h proj_api.h
@@ -28,16 +28,7 @@ find_path(PROJ4_INCLUDE_DIR
 find_file(PROJ4_HEADER
         NAMES proj.h proj_api.h
         PATHS
-          ${CMAKE_PREFIX_PATH}
-          $ENV{EXTERNLIBS}
-          $ENV{EXTERNLIBS}/proj4
-          ~/Library/Frameworks
-          /Library/Frameworks
-          /usr/local
-          /usr
-          /opt
-        PATH_SUFFIXES
-          include
+          ${PROJ4_INCLUDE_DIR}
         DOC "PROJ4 - Headers file"
         )
 
@@ -66,10 +57,17 @@ find_package_handle_standard_args(PROJ4 DEFAULT_MSG PROJ4_LIBRARY PROJ4_INCLUDE_
 
 mark_as_advanced(PROJ4_LIBRARY PROJ4_INCLUDE_DIR)
 
-if(PROJ4_FOUND)
+if (PROJ4_FOUND)
   set(PROJ4_INCLUDE_DIRS ${PROJ4_INCLUDE_DIR})
   set(PROJ4_HEADER_NAME PROJ4_HEADER_NAME-NOTFOUND)
   string(LENGTH ${PROJ4_INCLUDE_DIR} INCLUDE_PATH_LENGTH)
   MATH(EXPR INCLUDE_PATH_LENGTH "${INCLUDE_PATH_LENGTH}+1")
-  string(SUBSTRING ${PROJ4_HEADER} ${INCLUDE_PATH_LENGTH} -1 PROJ4_HEADER_NAME)
-endif(PROJ4_FOUND)
+  string(FIND ${PROJ4_HEADER} ${PROJ4_INCLUDE_DIR} PROJ4_HEADER_SAME_PATH)
+  if (PROJ4_HEADER_SAME_PATH > -1)
+    string(SUBSTRING ${PROJ4_HEADER} ${INCLUDE_PATH_LENGTH} -1 PROJ4_HEADER_NAME)
+  else ()
+    message(FATAL_ERROR "The path to Proj header file and the include directory do not match:\n
+            PROJ4_HEADER=${PROJ4_HEADER} and \n
+            PROJ4_INCLUDE_DIR=${PROJ4_INCLUDE_DIR}")
+  endif (PROJ4_HEADER_SAME_PATH)
+endif (PROJ4_FOUND)
