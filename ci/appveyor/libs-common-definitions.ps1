2d13f8bd1cfa117c911c8aa7f16011be3e36d7ae
Pascal Horton <pascal.horton@giub.unibe.ch>
2019-11-27 10:16:32 +0100

    Updating build process on AppVeyor.

--- ci/appveyor/libs-common-definitions.ps1
+++ ci/appveyor/libs-common-definitions.ps1
@@ -1,16 +1,16 @@
 # Options
 if ($env:APPVEYOR) {
-  $MSC_VER=1916
-  $VS_VER_NB="15"
-  $VS_VER_YR="2017"
-  $CMAKE_GENERATOR=""
+  $MSC_VER=1923
+  $VS_VER_NB="16"
+  $VS_VER_YR="2019"
+  $CMAKE_GENERATOR="-Ax64"
   $TMP_DIR="C:\projects\tmp"
   $LIB_DIR="C:\projects\libs"
   $CMAKE_DIR="C:\projects\cmake"
   $WIX_DIR="C:\projects\wix"
   $PATCH_DIR="C:\projects\atmoswing\ci\appveyor\patches"
 } else {
-  $MSC_VER=1920
+  $MSC_VER=1923
   $VS_VER_NB="16"
   $VS_VER_YR="2019"
   $CMAKE_GENERATOR="-Ax64"
@@ -19,6 +19,11 @@ if ($env:APPVEYOR) {
   $CMAKE_DIR="C:\Program Files\CMake\bin"
   $WIX_DIR="C:\Program Files\WiX"
   $PATCH_DIR=".\patches"
+}
+
+$VS_VER="Visual Studio $VS_VER_NB $VS_VER_YR Win64"
+if ($VS_VER_YR -ge "2019") {
+  $VS_VER="Visual Studio $VS_VER_NB $VS_VER_YR"
 }
 
 # Force rebuilding some libraries
@@ -42,10 +47,10 @@ $JPEG_URL="https://github.com/LuaDist/libjpeg/archive/master.zip"
 $PNG_URL="https://github.com/atmoswing/large-files/raw/master/libraries/libpng-1634.zip"
 $JASPER_URL="https://github.com/mdadams/jasper/archive/version-2.0.16.zip"
 $CURL_URL="https://github.com/curl/curl/archive/curl-7_64_1.zip"
-$PROJ_URL="https://github.com/OSGeo/proj.4/archive/6.2.0.zip"
+$PROJ_URL="https://github.com/OSGeo/PROJ/releases/download/6.2.1/proj-6.2.1.zip"
 $HDF5_URL="https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.5/src/CMake-hdf5-1.10.5.zip"
-$NETCDF_URL="https://github.com/Unidata/netcdf-c/archive/v4.7.2.zip"
-$GDAL_URL="https://github.com/OSGeo/gdal/releases/download/v3.0.1/gdal301.zip"
+$NETCDF_URL="https://github.com/Unidata/netcdf-c/archive/v4.7.3.zip"
+$GDAL_URL="https://github.com/OSGeo/gdal/releases/download/v3.0.2/gdal302.zip"
 $ECCODES_URL="https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.14.1-Source.tar.gz"
 $SQLITE_SRC_URL="https://www.sqlite.org/2019/sqlite-amalgamation-3270200.zip"
 $SQLITE_DLL_URL="https://www.sqlite.org/2019/sqlite-dll-win64-x64-3270200.zip"
