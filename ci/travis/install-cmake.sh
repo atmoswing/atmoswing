d3f1f465ba94ca782970d8fa8dfa9c786cfb6a89
Pascal Horton <pascal.horton@giub.unibe.ch>
2020-03-17 13:51:56 +0100

    Upgrading CMake on Travis.

---
+++ ci/travis/install-cmake.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+sudo apt purge --auto-remove -y cmake
+sudo find /usr/local -type f -name "cmake" -exec rm -rf {} \;
+wget https://github.com/Kitware/CMake/releases/download/v3.17.0-rc3/cmake-3.17.0-rc3-Linux-x86_64.sh
+sudo mkdir /opt/cmake
+sudo sh cmake-3.17.0-rc3-Linux-x86_64.sh --skip-license --prefix=/opt/cmake
+sudo ln -s /opt/cmake/bin/cmake /usr/bin/cmake
+sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
