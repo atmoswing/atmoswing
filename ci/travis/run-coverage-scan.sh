cfc560ce1c21ba3bffbf66d6fd91eeccfd2b0a24
Pascal Horton <pascal.horton@terranum.ch>
2020-03-18 23:07:57 +0100

    Decreasing log from coverage scan.

--- ci/travis/run-coverage-scan.sh
+++ ci/travis/run-coverage-scan.sh
@@ -3,6 +3,6 @@
 cmake CMakeLists.txt -DBUILD_OPTIMIZER=1 -DBUILD_FORECASTER=1 -DBUILD_DOWNSCALER=1 -DBUILD_VIEWER=0 -DUSE_GUI=0 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_CODECOV=1 -DGDAL_PATH=$HOME/.libs -DCMAKE_PREFIX_PATH=$HOME/.libs
 make -j$(nproc) atmoswing-coverage > /dev/null
 lcov --directory . --capture --output-file coverage.info &> /dev/null
-lcov --remove coverage.info '/usr/*' '*/external/*' '*/tests/*' --output-file coverage.info --quiet
-lcov --list coverage.info
+lcov --remove coverage.info '/usr/*' '*/external/*' '*/tests/*' --output-file coverage.info --quiet &> /dev/null
+lcov --list coverage.info &> /dev/null
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports" 
