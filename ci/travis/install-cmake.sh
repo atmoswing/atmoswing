85f27bdb502660641b5e36926541efd3e0cf589e
Pascal Horton <pascal.horton@giub.unibe.ch>
2020-04-09 15:45:12 +0200

    Working on Dockerfile.

--- ci/travis/install-cmake.sh
+++ ci/travis/install-cmake.sh
@@ -2,8 +2,8 @@
 
 sudo apt purge --auto-remove -y cmake
 sudo find /usr/local -type f -name "cmake" -exec rm -rf {} \;
-wget https://github.com/Kitware/CMake/releases/download/v3.17.0-rc3/cmake-3.17.0-rc3-Linux-x86_64.sh > /dev/null
+wget https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0-Linux-x86_64.sh > /dev/null
 sudo mkdir /opt/cmake
-sudo sh cmake-3.17.0-rc3-Linux-x86_64.sh --skip-license --prefix=/opt/cmake > /dev/null
+sudo sh cmake-3.17.0-Linux-x86_64.sh --skip-license --prefix=/opt/cmake > /dev/null
 sudo ln -s /opt/cmake/bin/cmake /usr/bin/cmake
 sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
