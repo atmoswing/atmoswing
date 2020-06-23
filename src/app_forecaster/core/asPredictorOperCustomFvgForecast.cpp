2020e87177ba96f2809f142fbc96b998dd8578c6
Pascal Horton <pascal.horton@giub.unibe.ch>
2020-06-23 17:27:54 +0200

    Testing operational FVG predictors.

--- src/app_forecaster/core/asPredictorOperCustomFvgForecast.cpp
+++ src/app_forecaster/core/asPredictorOperCustomFvgForecast.cpp
@@ -39,124 +39,104 @@ asPredictorOperCustomFvgForecast::asPredictorOperCustomFvgForecast(const wxStrin
 
 bool asPredictorOperCustomFvgForecast::Init() {
 
-    if (m_product.IsSameAs("data", false)) {
-        if (m_dataId.Contains("gh")) {
-            m_parameter = GeopotentialHeight;
-            m_gribCode = {0, 128, 156, 100};
-            m_unit = m;
-        } else if (m_dataId.Contains("t")) {
-            m_parameter = AirTemperature;
-            m_gribCode = {0, 128, 130, 100};
-            m_unit = degK;
-        } else if (m_dataId.Contains("w")) {
-            m_parameter = VerticalVelocity;
-            m_gribCode = {0, 128, 135, 100};
-            m_unit = Pa_s;
-        } else if (m_dataId.Contains("r")) {
-            m_parameter = RelativeHumidity;
-            m_gribCode = {0, 128, 157, 100};
-            m_unit = percent;
-        } else if (m_dataId.Contains("u")) {
-            m_parameter = Uwind;
-            m_gribCode = {0, 128, 131, 100};
-            m_unit = m_s;
-        } else if (m_dataId.Contains("v")) {
-            m_parameter = Vwind;
-            m_gribCode = {0, 128, 132, 100};
-            m_unit = m_s;
-        } else if (m_dataId.Contains("2t_sfc")) {
-            m_parameter = AirTemperature;
-            m_gribCode = {0, 128, 167, 1};
-            m_unit = degK;
-        } else if (m_dataId.Contains("10u_sfc")) {
-            m_parameter = Uwind;
-            m_gribCode = {0, 128, 165, 1};
-            m_unit = m_s;
-        } else if (m_dataId.Contains("10v_sfc")) {
-            m_parameter = Vwind;
-            m_gribCode = {0, 128, 166, 1};
-            m_unit = m_s;
-        } else if (m_dataId.Contains("cp_sfc")) {
-            m_parameter = Precipitation;
-            m_gribCode = {0, 128, 143, 1};
-            m_unit = m;
-        } else if (m_dataId.Contains("msl_sfc")) {
-            m_parameter = Pressure;
-            m_gribCode = {0, 128, 151, 1};
-            m_unit = Pa;
-        } else if (m_dataId.Contains("tp_sfc")) {
-            m_parameter = AirTemperature;
-            m_gribCode = {0, 128, 228, 1};
-            m_unit = degK;
-        } else {
-            if (m_parameter == ParameterUndefined) {
-                wxLogError(_("No '%s' parameter identified for the provided level type (%s)."), m_dataId, m_product);
-                return false;
-            }
-        }
+    m_parameter = Other;
 
-        m_fileNamePattern = m_dataId + ".%4d%02d%02d%02d.grib";
+    if (m_dataId.IsSameAs("DP500925", false)) {
+        m_gribCode = {0, 3, 113, 100};
+    } else if (m_dataId.IsSameAs("LRT700500", false)) {
+        m_gribCode = {0, 128, 130, 100};
+    } else if (m_dataId.IsSameAs("LRT850500", false)) {
+        m_gribCode = {0, 128, 130, 100};
+    } else if (m_dataId.IsSameAs("LRTE700500", false)) {
+        m_gribCode = {0, 3, 113, 100};
+    } else if (m_dataId.IsSameAs("LRTE850500", false)) {
+        m_gribCode = {0, 3, 113, 100};
+    } else if (m_dataId.IsSameAs("MB500850", false)) {
+        m_parameter = MaximumBuoyancy;
+        m_gribCode = {0, 3, 114, 100};
+    } else if (m_dataId.IsSameAs("MB500925", false)) {
+        m_parameter = MaximumBuoyancy;
+        m_gribCode = {0, 3, 114, 100};
+    } else if (m_dataId.IsSameAs("MB700925", false)) {
+        m_parameter = MaximumBuoyancy;
+        m_gribCode = {0, 3, 114, 100};
+    } else if (m_dataId.IsSameAs("MB850500", false)) {
+        m_parameter = MaximumBuoyancy;
+        m_gribCode = {0, 3, 114, 100};
+    } else if (m_dataId.Contains("thetaES")) {
+        m_parameter = PotentialTemperature;
+        m_gribCode = {0, 3, 114, 100};
+        m_unit = W_m2;
+    } else if (m_dataId.Contains("thetaE")) {
+        m_parameter = PotentialTemperature;
+        m_gribCode = {0, 3, 113, 100};
+        m_unit = W_m2;
+    } else if (m_dataId.Contains("vflux")) {
+        m_parameter = MomentumFlux;
+        m_gribCode = {0, 3, 125, 100};
+        m_unit = kg_m2_s;
+    } else if (m_dataId.Contains("uflux")) {
+        m_parameter = MomentumFlux;
+        m_gribCode = {0, 3, 124, 100};
+        m_unit = kg_m2_s;
+    } else if (m_dataId.Contains("2t_sfc")) {
+        m_parameter = AirTemperature;
+        m_gribCode = {0, 128, 167, 1};
+        m_unit = degK;
+    } else if (m_dataId.Contains("10u_sfc")) {
+        m_parameter = Uwind;
+        m_gribCode = {0, 128, 165, 1};
+        m_unit = m_s;
+    } else if (m_dataId.Contains("10v_sfc")) {
+        m_parameter = Vwind;
+        m_gribCode = {0, 128, 166, 1};
+        m_unit = m_s;
+    } else if (m_dataId.Contains("cp_sfc")) {
+        m_parameter = Precipitation;
+        m_gribCode = {0, 128, 143, 1};
+        m_unit = m;
+    } else if (m_dataId.Contains("msl_sfc")) {
+        m_parameter = Pressure;
+        m_gribCode = {0, 128, 151, 1};
+        m_unit = Pa;
+    } else if (m_dataId.Contains("tp_sfc")) {
+        m_parameter = AirTemperature;
+        m_gribCode = {0, 128, 228, 1};
+        m_unit = degK;
+    } else if (m_dataId.Contains("q")) {
+        m_parameter = SpecificHumidity;
+        m_gribCode = {0, 128, 133, 100};
+        m_unit = percent;
+    } else if (m_dataId.Contains("gh")) {
+        m_parameter = GeopotentialHeight;
+        m_gribCode = {0, 128, 156, 100};
+        m_unit = m;
+    } else if (m_dataId.Contains("t")) {
+        m_parameter = AirTemperature;
+        m_gribCode = {0, 128, 130, 100};
+        m_unit = degK;
+    } else if (m_dataId.Contains("w")) {
+        m_parameter = VerticalVelocity;
+        m_gribCode = {0, 128, 135, 100};
+        m_unit = Pa_s;
+    } else if (m_dataId.Contains("r")) {
+        m_parameter = RelativeHumidity;
+        m_gribCode = {0, 128, 157, 100};
+        m_unit = percent;
+    } else if (m_dataId.Contains("u")) {
+        m_parameter = Uwind;
+        m_gribCode = {0, 128, 131, 100};
+        m_unit = m_s;
+    } else if (m_dataId.Contains("v")) {
+        m_parameter = Vwind;
+        m_gribCode = {0, 128, 132, 100};
+        m_unit = m_s;
+    } else {
+        wxLogError(_("No '%s' parameter identified."), m_dataId);
+        return false;
+    } else
 
-    } else if (m_product.IsSameAs("datader", false)) {
-        if (m_dataId.Contains("thetaES")) {
-            m_parameter = PotentialTemperature;
-            m_gribCode = {0, 3, 114, 100};
-            m_unit = W_m2;
-        } else if (m_dataId.Contains("thetaE")) {
-            m_parameter = PotentialTemperature;
-            m_gribCode = {0, 3, 113, 100};
-            m_unit = W_m2;
-        } else if (m_dataId.Contains("vflux")) {
-            m_parameter = MomentumFlux;
-            m_gribCode = {0, 3, 125, 100};
-            m_unit = kg_m2_s;
-        } else if (m_dataId.Contains("uflux")) {
-            m_parameter = MomentumFlux;
-            m_gribCode = {0, 3, 124, 100};
-            m_unit = kg_m2_s;
-        } else if (m_dataId.Contains("q")) {
-            m_parameter = SpecificHumidity;
-            m_gribCode = {0, 128, 133, 100};
-            m_unit = percent;
-        } else {
-            wxLogError(_("No '%s' parameter identified for the provided level type (%s)."), m_dataId, m_product);
-            return false;
-        }
-
-        m_fileNamePattern = m_dataId + ".%4d%02d%02d%02d.grib";
-
-    } else if (m_product.IsSameAs("vertdiff", false)) {
-        m_parameter = Other;
-
-        if (m_dataId.IsSameAs("DP500925", false)) {
-            m_gribCode = {0, 3, 113, 100};
-        } else if (m_dataId.IsSameAs("LRT700500", false)) {
-            m_gribCode = {0, 128, 130, 100};
-        } else if (m_dataId.IsSameAs("LRT850500", false)) {
-            m_gribCode = {0, 128, 130, 100};
-        } else if (m_dataId.IsSameAs("LRTE700500", false)) {
-            m_gribCode = {0, 3, 113, 100};
-        } else if (m_dataId.IsSameAs("LRTE850500", false)) {
-            m_gribCode = {0, 3, 113, 100};
-        } else if (m_dataId.IsSameAs("MB500850", false)) {
-            m_parameter = MaximumBuoyancy;
-            m_gribCode = {0, 3, 114, 100};
-        } else if (m_dataId.IsSameAs("MB500925", false)) {
-            m_parameter = MaximumBuoyancy;
-            m_gribCode = {0, 3, 114, 100};
-        } else if (m_dataId.IsSameAs("MB700925", false)) {
-            m_parameter = MaximumBuoyancy;
-            m_gribCode = {0, 3, 114, 100};
-        } else if (m_dataId.IsSameAs("MB850500", false)) {
-            m_parameter = MaximumBuoyancy;
-            m_gribCode = {0, 3, 114, 100};
-        } else {
-            wxLogError(_("No '%s' parameter identified for the provided level type (%s)."), m_dataId, m_product);
-            return false;
-        }
-
-        m_fileNamePattern = m_dataId + ".%4d%02d%02d%02d.grib";
-    }
+    m_fileNamePattern = m_dataId + ".%4d%02d%02d%02d.grib";
 
     // Set to initialized
     m_initialized = true;
---
+++ tests/src/asPredictorOperCustomMeteoFvgTest.cpp
@@ -0,0 +1,315 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can read the License at http://opensource.org/licenses/CDDL-1.0
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in
+ * each file and include the License file (licence.txt). If applicable,
+ * add the following below this CDDL Header, with the fields enclosed
+ * by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * The Original Software is AtmoSwing.
+ * The Original Software was developed at the University of Lausanne.
+ * All Rights Reserved.
+ *
+ */
+
+/*
+ * Portions Copyright 2019 Pascal Horton, University of Bern.
+ */
+
+#include <wx/filename.h>
+
+#include <gtest/gtest.h>
+#include "asAreaCompGrid.h"
+#include "asPredictorOper.h"
+#include "asTimeArray.h"
+
+TEST(PredictorOperCustomMeteoFvg, GetCorrectPredictors) {
+    asPredictorOper *predictor;
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "DP500925");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Other);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "LRT700500");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Other);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "LRT850500");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Other);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "LRTE700500");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Other);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "LRTE850500");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Other);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "MB500850");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::MaximumBuoyancy);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "MB500925");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::MaximumBuoyancy);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "MB700925");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::MaximumBuoyancy);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "MB850500");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::MaximumBuoyancy);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "thetaES");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::PotentialTemperature);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "thetaE");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::PotentialTemperature);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "vflux");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::MomentumFlux);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "uflux");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::MomentumFlux);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "2t_sfc");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::AirTemperature);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "10u_sfc");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Uwind);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "10v_sfc");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Vwind);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "cp_sfc");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Precipitation);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "msl_sfc");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Pressure);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "tp_sfc");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::AirTemperature);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "q");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::SpecificHumidity);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "gh");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::GeopotentialHeight);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "t");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::AirTemperature);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "w");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::VerticalVelocity);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "r");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::RelativeHumidity);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "u");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Uwind);
+    wxDELETE(predictor);
+
+    predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "v");
+    ASSERT_TRUE(predictor->GetParameter() == asPredictor::Vwind);
+    wxDELETE(predictor);
+
+}
+
+TEST(PredictorOperCustomMeteoFvg, LoadSingleDay) {
+    vwxs filepaths;
+    filepaths.push_back(wxFileName::GetCwd() + "/files/data-custom-meteo-fvg-operational/MB500925.2020062106.grib");
+
+    asTimeArray dates(asTime::GetMJD(2020, 6, 21, 06), asTime::GetMJD(2020, 6, 21, 06), 6, "Simple");
+    dates.Init();
+
+    double xMin = 6;
+    int xPtsNb = 6;
+    double yMin = 45;
+    int yPtsNb = 5;
+    double step = 0.125;
+    float level = 0;
+    wxString gridType = "Regular";
+    asAreaCompGrid *area = asAreaCompGrid::GetInstance(gridType, xMin, xPtsNb, step, yMin, yPtsNb, step);
+
+    asPredictorOper *predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "MB500925");
+    wxASSERT(predictor);
+
+    // Create file names
+    predictor->SetFileNames(filepaths);
+
+    // Load
+    ASSERT_TRUE(predictor->Load(area, dates, level));
+
+    vva2f data = predictor->GetData();
+    // hgt[time][mem](lat,lon)
+
+    /* Values time 06h (horizontal=Lon, vertical=Lat)
+    Extracted:
+    10.919	9.278	7.830	7.512	7.608	6.612
+    9.954	9.131	8.042	7.555	7.725	7.921
+    8.371	8.455	8.475	8.526	9.071	9.790
+    8.760	8.659	8.982	8.683	9.494	10.531
+    9.836	10.344	10.228	9.200	9.229	9.848
+    */
+    EXPECT_NEAR(10.919, data[0][0](0, 0), 0.002);
+    EXPECT_NEAR(9.278, data[0][0](0, 1), 0.002);
+    EXPECT_NEAR(7.830, data[0][0](0, 2), 0.002);
+    EXPECT_NEAR(7.512, data[0][0](0, 3), 0.002);
+    EXPECT_NEAR(7.608, data[0][0](0, 4), 0.002);
+    EXPECT_NEAR(6.612, data[0][0](0, 5), 0.002);
+    EXPECT_NEAR(9.954, data[0][0](1, 0), 0.002);
+    EXPECT_NEAR(8.371, data[0][0](2, 0), 0.002);
+    EXPECT_NEAR(9.836, data[0][0](4, 0), 0.002);
+    EXPECT_NEAR(9.848, data[0][0](4, 5), 0.002);
+
+    wxDELETE(area);
+    wxDELETE(predictor);
+}
+
+TEST(PredictorOperCustomMeteoFvg, LoadLastTimeStep) {
+    vwxs filepaths;
+    filepaths.push_back(wxFileName::GetCwd() + "/files/data-custom-meteo-fvg-operational/MB500925.2020062190.grib");
+
+    asTimeArray dates(asTime::GetMJD(2020, 6, 24, 18), asTime::GetMJD(2020, 6, 24, 18), 6, "Simple");
+    dates.Init();
+
+    double xMin = 6;
+    int xPtsNb = 6;
+    double yMin = 45;
+    int yPtsNb = 5;
+    double step = 0.125;
+    float level = 0;
+    wxString gridType = "Regular";
+    asAreaCompGrid *area = asAreaCompGrid::GetInstance(gridType, xMin, xPtsNb, step, yMin, yPtsNb, step);
+
+    asPredictorOper *predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "MB500925");
+    wxASSERT(predictor);
+
+    // Create file names
+    predictor->SetFileNames(filepaths);
+
+    // Load
+    ASSERT_TRUE(predictor->Load(area, dates, level));
+
+    vva2f data = predictor->GetData();
+    // hgt[time][mem](lat,lon)
+
+    /* Values time 90h (horizontal=Lon, vertical=Lat)
+    Extracted:
+    -7.7	-10.0	-13.9	-15.9	-14.1	-10.7
+    -12.2	-13.0	-14.8	-14.5	-11.0	-8.1
+    -14.2	-13.3	-12.7	-12.0	-9.7	-7.1
+    -15.7	-12.8	-9.8	-8.6	-9.0	-8.6
+    -14.8	-11.8	-7.0	-5.1	-7.3	-9.9
+    */
+    EXPECT_NEAR(-7.7, data[0][0](0, 0), 0.002);
+    EXPECT_NEAR(-10.0, data[0][0](0, 1), 0.002);
+    EXPECT_NEAR(-13.9, data[0][0](0, 2), 0.002);
+    EXPECT_NEAR(-15.9, data[0][0](0, 3), 0.002);
+    EXPECT_NEAR(-14.1, data[0][0](0, 4), 0.002);
+    EXPECT_NEAR(-10.7, data[0][0](0, 5), 0.002);
+    EXPECT_NEAR(-12.2, data[0][0](1, 0), 0.002);
+    EXPECT_NEAR(-14.2, data[0][0](2, 0), 0.002);
+    EXPECT_NEAR(-14.8, data[0][0](4, 0), 0.002);
+    EXPECT_NEAR(-9.9, data[0][0](4, 5), 0.002);
+
+    wxDELETE(area);
+    wxDELETE(predictor);
+}
+
+TEST(PredictorOperCustomMeteoFvg, LoadFullTimeArray) {
+    vwxs filepaths;
+    filepaths.push_back(wxFileName::GetCwd() + "/files/data-custom-meteo-fvg-operational/MB500925.2020062106.grib");
+    filepaths.push_back(wxFileName::GetCwd() + "/files/data-custom-meteo-fvg-operational/MB500925.2020062190.grib");
+
+    asTimeArray dates(asTime::GetMJD(2020, 6, 21, 06), asTime::GetMJD(2020, 6, 24, 18), 84, "Simple");
+    dates.Init();
+
+    double xMin = 6;
+    int xPtsNb = 6;
+    double yMin = 45;
+    int yPtsNb = 5;
+    double step = 0.125;
+    float level = 0;
+    wxString gridType = "Regular";
+    asAreaCompGrid *area = asAreaCompGrid::GetInstance(gridType, xMin, xPtsNb, step, yMin, yPtsNb, step);
+
+    asPredictorOper *predictor = asPredictorOper::GetInstance("Custom_MeteoFVG_Forecast", "MB500925");
+    wxASSERT(predictor);
+
+    // Create file names
+    predictor->SetFileNames(filepaths);
+
+    // Load
+    ASSERT_TRUE(predictor->Load(area, dates, level));
+
+    vva2f data = predictor->GetData();
+    // hgt[time][mem](lat,lon)
+
+    /* Values time step 0 (horizontal=Lon, vertical=Lat)
+    Extracted:
+    10.919	9.278	7.830	7.512	7.608	6.612
+    9.954	9.131	8.042	7.555	7.725	7.921
+    8.371	8.455	8.475	8.526	9.071	9.790
+    8.760	8.659	8.982	8.683	9.494	10.531
+    9.836	10.344	10.228	9.200	9.229	9.848
+    */
+    EXPECT_NEAR(10.919, data[0][0](0, 0), 0.002);
+    EXPECT_NEAR(9.278, data[0][0](0, 1), 0.002);
+    EXPECT_NEAR(7.830, data[0][0](0, 2), 0.002);
+    EXPECT_NEAR(7.512, data[0][0](0, 3), 0.002);
+    EXPECT_NEAR(7.608, data[0][0](0, 4), 0.002);
+    EXPECT_NEAR(6.612, data[0][0](0, 5), 0.002);
+    EXPECT_NEAR(9.954, data[0][0](1, 0), 0.002);
+    EXPECT_NEAR(8.371, data[0][0](2, 0), 0.002);
+    EXPECT_NEAR(9.836, data[0][0](4, 0), 0.002);
+    EXPECT_NEAR(9.848, data[0][0](4, 5), 0.002);
+
+    /* Values time 90h (horizontal=Lon, vertical=Lat)
+    Extracted:
+    -7.7	-10.0	-13.9	-15.9	-14.1	-10.7
+    -12.2	-13.0	-14.8	-14.5	-11.0	-8.1
+    -14.2	-13.3	-12.7	-12.0	-9.7	-7.1
+    -15.7	-12.8	-9.8	-8.6	-9.0	-8.6
+    -14.8	-11.8	-7.0	-5.1	-7.3	-9.9
+    */
+    EXPECT_NEAR(-7.7, data[1][0](0, 0), 0.002);
+    EXPECT_NEAR(-10.0, data[1][0](0, 1), 0.002);
+    EXPECT_NEAR(-13.9, data[1][0](0, 2), 0.002);
+    EXPECT_NEAR(-15.9, data[1][0](0, 3), 0.002);
+    EXPECT_NEAR(-14.1, data[1][0](0, 4), 0.002);
+    EXPECT_NEAR(-10.7, data[1][0](0, 5), 0.002);
+    EXPECT_NEAR(-12.2, data[1][0](1, 0), 0.002);
+    EXPECT_NEAR(-14.2, data[1][0](2, 0), 0.002);
+    EXPECT_NEAR(-14.8, data[1][0](4, 0), 0.002);
+    EXPECT_NEAR(-9.9, data[1][0](4, 5), 0.002);
+
+    wxDELETE(area);
+    wxDELETE(predictor);
+}
