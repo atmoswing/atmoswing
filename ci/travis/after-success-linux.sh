8547d46ee0d99dc0838e2045152b20a137dc3d4e
Pascal Horton <pascal.horton@giub.unibe.ch>
2020-10-07 13:30:40 +0200

    Debugging Travis deploy.

--- ci/travis/after-success-linux.sh
+++ ci/travis/after-success-linux.sh
@@ -3,6 +3,7 @@
 export LD_LIBRARY_PATH=$HOME/.libs/lib:$LD_LIBRARY_PATH
 cd $TRAVIS_BUILD_DIR
 cpack -C release -G DEB
+cat /home/travis/build/atmoswing/atmoswing/_CPack_Packages/Linux/DEB/PreinstallOutput.log
 ls -lha
 rename "s/linux64/${DISTRO}/" *linux64.deb
 export PKG_FILE=$(ls *.deb)
