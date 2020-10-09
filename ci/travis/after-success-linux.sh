01c559ffe116b3a065f50b20c4f7da32c7db91d8
Pascal Horton <pascal.horton@terranum.ch>
2020-10-09 14:37:21 +0200

    Working on Travis deploy.

--- ci/travis/after-success-linux.sh
+++ ci/travis/after-success-linux.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=$HOME/.libs/lib:$LD_LIBRARY_PATH
 cd /home/travis/build/atmoswing/atmoswing || exit
-cpack -C release -G DEB
+/usr/bin/cpack -C release -G DEB
 cat /home/travis/build/atmoswing/atmoswing/_CPack_Packages/Linux/DEB/PreinstallOutput.log
 ls -lha
 rename "s/linux64/${DISTRO}/" *linux64.deb
