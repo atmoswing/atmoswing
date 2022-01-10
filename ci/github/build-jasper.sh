1af5753a5e3ebfa687f4bb015da868d0c1592463
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-01-10 16:57:22 +0100

    Updating Jasper build script.

--- ci/github/build-jasper.sh
+++ ci/github/build-jasper.sh
@@ -8,7 +8,7 @@ tar -xzf jasper.tar.gz
 cd jasper-version-${JASPER_VERSION}
 mkdir bld
 cd bld
-cmake .. -DCMAKE_INSTALL_PREFIX=${HOME}/.libs -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="${HOME}/.libs"
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j $(nproc)
 make install
 cd ..
