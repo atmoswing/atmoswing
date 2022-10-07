02c866df6f523e01f5c245858ae9140ec7ffbf3a
Pascal Horton <pascal.horton@terranum.ch>
2022-10-07 16:29:56 +0200

    Removing former CI scripts.

--- ci/appveyor/make-installers.ps1
+++
@@ -1,8 +0,0 @@
-cd C:\projects\atmoswing\bin
-cpack -C release -G ZIP
-cpack -C release -G NSIS
-cpack -C release -G WIX
-cd C:\projects\atmoswing
-copy bin\*win64.exe .\
-copy bin\*win64.zip .\
-copy bin\*win64.msi .\
