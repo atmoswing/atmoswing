4f3afc06580a3ffbdecb96e09125f330fbc43ead
Pascal Horton <pascal.horton@terranum.ch>
2023-03-16 13:33:16 +0100

    Adding files for the predictors frame.

---
+++ src/app_viewer/gui/asFramePredictors.h
@@ -0,0 +1,54 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can read the License at http://opensource.org/licenses/CDDL-1.0
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in
+ * each file and include the License file (licence.txt). If applicable,
+ * add the following below this CDDL Header, with the fields enclosed
+ * by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * The Original Software is AtmoSwing.
+ * The Original Software was developed at the University of Lausanne.
+ * All Rights Reserved.
+ *
+ */
+
+/*
+ * Portions Copyright 2022-2023 Pascal Horton, Terranum.
+ */
+
+#ifndef AS_FRAME_PREDICTORS_H
+#define AS_FRAME_PREDICTORS_H
+
+#include "AtmoswingViewerGui.h"
+#include "asForecastManager.h"
+#include "asIncludes.h"
+#include "asPredictorsRenderer.h"
+
+class asFramePredictors : public asFramePredictorsVirtual {
+  public:
+    asFramePredictors(wxWindow* parent, int selectedForecast, asForecastManager* forecastManager,
+                      wxWindowID id = asWINDOW_PREDICTORS);
+
+    ~asFramePredictors();
+
+  protected:
+    wxKeyboardState m_KeyBoardState;
+
+  private:
+    asForecastManager* m_forecastManager;
+    asPredictorsRenderer* m_predictorsViewer;
+    asPredictorsManager* m_predictorsManager;
+
+    DECLARE_EVENT_TABLE()
+};
+
+#endif
---
+++ src/app_viewer/gui/asPredictorsRenderer.cpp
@@ -0,0 +1,50 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can read the License at http://opensource.org/licenses/CDDL-1.0
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in
+ * each file and include the License file (licence.txt). If applicable,
+ * add the following below this CDDL Header, with the fields enclosed
+ * by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * The Original Software is AtmoSwing.
+ * The Original Software was developed at the University of Lausanne.
+ * All Rights Reserved.
+ *
+ */
+
+/*
+ * Portions Copyright 2022-2023 Pascal Horton, Terranum.
+ */
+
+#include "asPredictorsRenderer.h"
+
+#include "asPredictorsManager.h"
+#include "vrlayervector.h"
+#include "vrrender.h"
+
+asPredictorsRenderer::asPredictorsRenderer(wxWindow* parent, vrLayerManager* layerManager,
+                                           asPredictorsManager* predictorsManager,
+                                           vrViewerLayerManager* viewerLayerManagerTarget,
+                                           vrViewerLayerManager* viewerLayerManagerAnalog,
+                                           wxCheckListBox* checkListPredictors) {
+    m_parent = parent;
+    m_layerManager = layerManager;
+    m_predictorsManager = predictorsManager;
+    m_viewerLayerManagerTarget = viewerLayerManagerTarget;
+    m_viewerLayerManagerAnalog = viewerLayerManagerAnalog;
+}
+
+asPredictorsRenderer::~asPredictorsRenderer() {}
+
+void asPredictorsRenderer::Redraw(double targetDate, double analogDate) {
+
+}
