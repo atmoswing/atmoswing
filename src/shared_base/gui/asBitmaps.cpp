2c63d8e3599b6036825f95dfb9d81e57a9cfccbc
Pascal Horton <pascal.horton@terranum.ch>
2023-05-15 10:23:57 +0200

    Replacing toolbar icons by svg versions.

---
+++ src/shared_base/gui/asBitmaps.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pascal on 15/05/2023.
+//
+
+#include "asBitmaps.h"
---
+++ src/shared_base/gui/asBitmaps.h
@@ -0,0 +1,10 @@
+//
+// Created by Pascal on 15/05/2023.
+//
+
+#ifndef ATMOSWING_ASBITMAPS_H
+#define ATMOSWING_ASBITMAPS_H
+
+class asBitmaps {};
+
+#endif  // ATMOSWING_ASBITMAPS_H
