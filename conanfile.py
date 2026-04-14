6090028386fbf6b9f34fcd0a49ec27ca1a1d97e4
Pascal Horton <pascal.horton@unibe.ch>
2026-04-14 09:44:10 +0200

    Migrating to vcpck.

--- conanfile.py
+++ conanfile.py
@@ -3,62 +3,6 @@ import os
 
 
 class AmtoSwing(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-
-    options = {
-        "enable_tests": [True, False],
-        "enable_benchmark": [True, False],
-        "code_coverage": [True, False],
-        "with_gui": [True, False],
-        "test_gui": [True, False],
-        "use_cuda": [True, False],
-        "build_forecaster": [True, False],
-        "build_viewer": [True, False],
-        "build_optimizer": [True, False],
-        "build_downscaler": [True, False],
-        "create_installer": [True, False],
-        "with_ssl": [True, False],  # for libcurl; can be disabled in case of problems
-    }
-    default_options = {
-        "enable_tests": True,
-        "enable_benchmark": False,
-        "code_coverage": False,
-        "with_gui": True,
-        "test_gui": False,
-        "use_cuda": False,
-        "build_forecaster": True,
-        "build_viewer": True,
-        "build_optimizer": True,
-        "build_downscaler": True,
-        "create_installer": False,
-        "with_ssl": True,
-    }
-
-    generators = "cmake", "gcc"
-
-    def requirements(self):
-        self.requires("proj/9.3.0")
-        self.requires("libcurl/8.4.0")
-        self.requires("libtiff/4.6.0")
-        self.requires("sqlite3/3.44.0")
-        self.requires("eigen/3.4.0")
-        self.requires("netcdf/4.8.1")
-        self.requires("libdeflate/1.19")
-        self.requires("libjpeg/9e")
-        self.requires("zlib/1.3")
-        self.requires("libpng/1.6.40")
-        self.requires("eccodes/2.27.0@terranum-conan+eccodes/stable")
-        if self.options.enable_tests or self.options.code_coverage:
-            self.requires("gtest/1.14.0")
-        if self.options.enable_benchmark:
-            self.requires("benchmark/1.8.3")
-            self.requires("gtest/1.14.0")
-        if self.options.with_gui:
-            self.requires("wxwidgets/3.2.2.1@terranum-conan+wxwidgets/stable")
-        else:
-            self.requires("wxbase/3.2.1@terranum-conan+wxbase/stable")
-        if self.options.build_viewer:
-            self.requires("gdal/3.7.0")
 
     def configure(self):
         if self.options.code_coverage:
@@ -123,33 +67,3 @@ class AmtoSwing(ConanFile):
         if self.settings.os == "Macos":
             self.copy("*.mo", dst="bin/AtmoSwing.app/Contents/Resources/fr.lproj",
                       src=os.path.join(_source_folder, "locales/fr"))
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = self.options.enable_tests
-        cmake.definitions["TEST_GUI"] = self.options.test_gui
-        cmake.definitions["BUILD_BENCHMARK"] = self.options.enable_benchmark
-        cmake.definitions["USE_CODECOV"] = self.options.code_coverage
-        cmake.definitions["USE_GUI"] = self.options.with_gui
-        cmake.definitions["USE_CUDA"] = self.options.use_cuda
-        cmake.definitions["BUILD_FORECASTER"] = self.options.build_forecaster
-        cmake.definitions["BUILD_VIEWER"] = self.options.build_viewer
-        cmake.definitions["BUILD_OPTIMIZER"] = self.options.build_optimizer
-        cmake.definitions["BUILD_DOWNSCALER"] = self.options.build_downscaler
-        cmake.definitions["CREATE_INSTALLER"] = self.options.create_installer
-
-        if self.options.code_coverage:
-            cmake.definitions["BUILD_TESTS"] = "ON"
-        if self.options.build_viewer:
-            self.options.with_gui = True
-            cmake.definitions["USE_GUI"] = "ON"
-        if self.settings.os == "Windows":
-            if self.options.with_gui:
-                cmake.definitions["wxWidgets_CONFIGURATION"] = "mswu"
-            else:
-                cmake.definitions["wxWidgets_CONFIGURATION"] = "baseu"
-
-        cmake.configure()
-        cmake.build()
-        if self.settings.os == "Macos":
-            cmake.install()
