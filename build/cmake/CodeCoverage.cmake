5c4461c965dd1daaf0f118fe978e8c93f95cb0c0
Pascal Horton <pascal.horton@terranum.ch>
2026-06-30 10:20:29 +0200

    Trying to fix the code coverage action.

--- build/cmake/CodeCoverage.cmake
+++ build/cmake/CodeCoverage.cmake
@@ -153,13 +153,13 @@ function(SETUP_TARGET_FOR_COVERAGE)
             # Cleanup lcov
             COMMAND ${LCOV_PATH} --directory . --zerocounters
             # Create baseline to make sure untouched files show up in the report
-            COMMAND ${LCOV_PATH} -c -i -d . -o ${Coverage_NAME}.base
+            COMMAND ${LCOV_PATH} -c -i -d . -o ${Coverage_NAME}.base --ignore-errors mismatch
 
             # Run tests
             COMMAND ${Coverage_EXECUTABLE}
 
             # Capturing lcov counters and generating report
-            COMMAND ${LCOV_PATH} --directory . --capture --output-file ${Coverage_NAME}.info
+            COMMAND ${LCOV_PATH} --directory . --capture --output-file ${Coverage_NAME}.info --ignore-errors mismatch
             # add baseline counters
             COMMAND ${LCOV_PATH} -a ${Coverage_NAME}.base -a ${Coverage_NAME}.info --output-file ${Coverage_NAME}.total
             COMMAND ${LCOV_PATH} --remove ${Coverage_NAME}.total ${COVERAGE_EXCLUDES} --output-file ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
